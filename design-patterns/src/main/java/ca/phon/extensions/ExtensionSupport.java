dd1ba0b78a665f46f31e8b1ead90bb27e0ed64ad
ghedlund <ghedlund@mun.ca>
2012-06-27 11:42:00 -0330

    Added clone of English syllabifier.

--- design-patterns/src/main/java/ca/phon/extensions/ExtensionSupport.java
+++ design-patterns/src/main/java/ca/phon/extensions/ExtensionSupport.java
@@ -120,14 +120,14 @@ public final class ExtensionSupport implements IExtendable {
 
 	@Override
 	public <T> T getExtension(Class<T> cap) {
-		Object inst = extensions.get(cap);
-		T retVal = cap.cast(inst);
+		final Object inst = extensions.get(cap);
+		final T retVal = cap.cast(inst);
 		return retVal;
 	}
 
 	@Override
 	public <T> T putExtension(Class<T> cap, T impl) {
-		Extension capAnnotation = 
+		final Extension capAnnotation = 
 				cap.getAnnotation(Extension.class);
 		if(capAnnotation == null || !capAnnotation.value().isAssignableFrom(declaredType)) {
 			throw new IllegalArgumentException("Class " + cap.getName() + 
