e541801f1a506a37d422169389bbb95ad1f9f6e6
Greg Hedlund <greg.hedlund@gmail.com>
2013-07-05 08:31:20 -0230

    Started default IPA dictionary provider.

---
+++ ipadictionary/src/main/java/ca/phon/ipadictionary/DefaultDictionaryProvider.java
@@ -0,0 +1,16 @@
+package ca.phon.ipadictionary;
+
+import java.io.IOException;
+import java.net.URL;
+
+import ca.phon.util.resources.ClassLoaderHandler;
+
+public class DefaultDictionaryProvider extends ClassLoaderHandler<IPADictionary> {
+
+	@Override
+	public IPADictionary loadFromURL(URL url) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ ipadictionary/src/main/java/ca/phon/ipadictionary/IPADictionaryLibrary.java
@@ -0,0 +1,63 @@
+package ca.phon.ipadictionary;
+
+import java.util.Set;
+
+import ca.phon.extensions.ExtensionSupport;
+import ca.phon.extensions.IExtendable;
+import ca.phon.util.resources.ResourceLoader;
+
+/**
+ * Manages the library of available IPA dictionaries.
+ */
+public class IPADictionaryLibrary implements IExtendable {
+	
+	/**
+	 * Extension support
+	 */
+	private final ExtensionSupport extSupport = new ExtensionSupport(IPADictionaryLibrary.class, this);
+
+	/**
+	 * Resource loader
+	 */
+	private final ResourceLoader<IPADictionary> resLoader = 
+			new ResourceLoader<IPADictionary>();
+	
+	public IPADictionaryLibrary() {
+		setupLoader();
+	}
+	
+	private void setupLoader() {
+		// add the default dictionary handler
+		
+	}
+	
+	/**
+	 * Get the loader used with the library.
+	 * 
+	 * @return the resource loader
+	 */
+	public ResourceLoader<IPADictionary> getLoader() {
+		return this.resLoader;
+	}
+
+	@Override
+	public Set<Class<?>> getExtensions() {
+		return extSupport.getExtensions();
+	}
+	
+	@Override
+	public <T> T getExtension(Class<T> cap) {
+		return extSupport.getExtension(cap);
+	}
+
+	@Override
+	public <T> T putExtension(Class<T> cap, T impl) {
+		return extSupport.putExtension(cap, impl);
+	}
+
+	@Override
+	public <T> T removeExtension(Class<T> cap) {
+		return extSupport.removeExtension(cap);
+	}
+
+}
