63597696c589e0496350aaa19b745c619d9e10c2
Greg Hedlund <greg.hedlund@gmail.com>
2013-10-07 08:10:12 -0230

    Minor changes to tests files

---
+++ ipa/src/test/java/ca/phon/ipa/TestPhonex.java
@@ -0,0 +1,26 @@
+package ca.phon.ipa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import ca.phon.phonex.PhonexMatcher;
+import ca.phon.phonex.PhonexPattern;
+
+@RunWith(JUnit4.class)
+public class TestPhonex {
+
+	@Test
+	public void testPhonex() throws Exception {
+		final String ipa = "ˈbʌɹθˌd";
+		final String phonex = "\\c*\\v+";
+		
+		final PhonexPattern pattern = PhonexPattern.compile(phonex);
+		final PhonexMatcher matcher = pattern.matcher(IPATranscript.parseTranscript(ipa));
+		int cIdx = 0;
+		while(matcher.find()) {
+			System.out.println(matcher.group());
+		}
+	}
+	
+}
