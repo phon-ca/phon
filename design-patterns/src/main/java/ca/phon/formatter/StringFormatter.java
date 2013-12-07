3b34ff8d3e07e9f05f87ed86f092d3e7b4165ce7
Greg Hedlund <greg.hedlund@gmail.com>
2013-12-07 11:22:20 -0330

    Added default String formatter

---
+++ design-patterns/src/main/java/ca/phon/formatter/StringFormatter.java
@@ -0,0 +1,24 @@
+package ca.phon.formatter;
+
+import java.text.ParseException;
+
+/**
+ * Basic formatter which simply returns the given string
+ * value.
+ * 
+ * 
+ */
+@FormatterType(String.class)
+public class StringFormatter implements Formatter {
+
+	@Override
+	public String format(Object obj) {
+		return obj.toString();
+	}
+
+	@Override
+	public Object parse(String text) throws ParseException {
+		return text;
+	}
+	
+}
