504a995817e9bba1fd6f1739712b24751addba77
Greg Hedlund <ghedlund@mun.ca>
2014-04-16 11:36:49 -0230

    Updated run_phon.sh script.

--- run_phon.sh
+++ run_phon.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-java -Xms250m -Xmx1024m -Xdock:name=Phon -Xdock:icon=app/src/main/resources/data/icons/Phon.icns -cp "app/target/phon-app-1.7.0-SNAPSHOT.jar:app/target/deps/*" -Dswing.aatext=true -Dcom.apple.mrj.application.apple.menu.about.name=Phon -Dcom.apple.macos.smallTabs=truei \
+JAVA_HOME=`/usr/libexec/java_home -v1.6` java -Xms250m -Xmx1024m -Xdock:name=Phon -Xdock:icon=app/src/main/resources/data/icons/Phon.icns -cp "app/target/phon-app-1.7.0-SNAPSHOT.jar:app/target/deps/*" -Dswing.aatext=true -Dcom.apple.mrj.application.apple.menu.about.name=Phon -Dcom.apple.macos.smallTabs=truei \
 -Dapple.laf.useScreenMenuBar=true \
 -Dcom.apple.mrj.application.live-resize=true \
 -Dapple.awt.textantialiasing=on \
--Dapple.awt.graphics.UseQuartz=true \
 -Dapple.awt.showGrowBox=true \
 -Dfile.encoding=UTF-8 \
 -Dsun.jnu.encoding=UTF-8 \
