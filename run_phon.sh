793021896d980047cf7169adbe7d43bc822f42ea
Greg Hedlund <ghedlund@mun.ca>
2014-04-17 07:47:56 -0230

    Updated run_phon script

--- run_phon.sh
+++ run_phon.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-JAVA_HOME=`/usr/libexec/java_home -v1.6` java -Xms250m -Xmx1024m -Xdock:name=Phon -Xdock:icon=app/src/main/resources/data/icons/Phon.icns -cp "app/target/phon-app-1.7.0-SNAPSHOT.jar:app/target/deps/*" -Dswing.aatext=true -Dcom.apple.mrj.application.apple.menu.about.name=Phon -Dcom.apple.macos.smallTabs=truei \
+JAVA_HOME=`/usr/libexec/java_home -v1.6` \
+java -Xms250m -Xmx1024m \
+-Xdock:name=Phon \
+-Xdock:icon=app/src/main/resources/data/icons/Phon.icns \
+-cp "app/target/phon-app-1.7.0-SNAPSHOT.jar:app/target/deps/*" \
+-Dswing.aatext=true \
+-Dcom.apple.mrj.application.apple.menu.about.name=Phon \
+-Dcom.apple.macos.smallTabs=true \
 -Dapple.laf.useScreenMenuBar=true \
 -Dcom.apple.mrj.application.live-resize=true \
 -Dapple.awt.textantialiasing=on \
@@ -8,5 +15,6 @@ JAVA_HOME=`/usr/libexec/java_home -v1.6` java -Xms250m -Xmx1024m -Xdock:name=Pho
 -Dfile.encoding=UTF-8 \
 -Dsun.jnu.encoding=UTF-8 \
 -Dapple.laf.useScreenMenuBar=true \
+$PHON_OPTS \
 ca.phon.app.Main 2>&1 | tee phon.log
 
