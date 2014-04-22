9e192761cdbf29c671d52cb70458031b7ae63841
Gregory Hedlund <ghedlund@mun.ca>
2014-04-22 10:19:56 -0230

    Added ability to track stage results in BasicSyllabifier

--- syllabifier/src/main/java/ca/phon/syllabifier/basic/BasicSyllabifier.java
+++ syllabifier/src/main/java/ca/phon/syllabifier/basic/BasicSyllabifier.java
@@ -17,11 +17,15 @@
  */
 package ca.phon.syllabifier.basic;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 
 import ca.phon.extensions.Extension;
+import ca.phon.extensions.ExtensionSupport;
+import ca.phon.extensions.IExtendable;
 import ca.phon.ipa.IPAElement;
 import ca.phon.ipa.IPATranscript;
 import ca.phon.syllabifier.Syllabifier;
@@ -29,6 +33,7 @@ import ca.phon.syllabifier.basic.io.StageType;
 import ca.phon.syllabifier.basic.io.SyllabifierDef;
 import ca.phon.syllable.SyllableConstituentType;
 import ca.phon.util.Language;
+import ca.phon.util.PrefHelper;
 
 /**
  * 
@@ -37,20 +42,27 @@ import ca.phon.util.Language;
  * 
  *
  */
-public class BasicSyllabifier implements Syllabifier {
+public class BasicSyllabifier implements Syllabifier, IExtendable {
 	
+	public final static String TRACK_STAGES_PROP = BasicSyllabifier.class.getName() + ".trackStages";
+	private final boolean trackStages = PrefHelper.getBoolean(TRACK_STAGES_PROP, Boolean.FALSE);
+	
+	@Extension(IPAElement.class)
+	public static class SyllabifierStageResults {
+		final Map<String, String> stages = new LinkedHashMap<String, String>();
+	}
+	
+	
+	private final ExtensionSupport extSupport = new ExtensionSupport(BasicSyllabifier.class, this);
+
 	private final SyllabifierDef def;
 	
 	/** Constructor */
 	BasicSyllabifier(SyllabifierDef def) {
 		super();
 		
+		extSupport.initExtensions();
 		this.def = def;
-	}
-	
-	@Extension(IPAElement.class)
-	public static class SyllabifierStageResults {
-		final Map<String, IPATranscript> stages = new TreeMap<String, IPATranscript>();
 	}
 
 	@Override
@@ -66,6 +78,15 @@ public class BasicSyllabifier implements Syllabifier {
 	@Override
 	public void syllabify(List<IPAElement> phones) {
 		final SonorityScale scale = new SonorityScale(def.getSonorityScale());
+		SyllabifierStageResults stageResults = null;
+		
+		if(trackStages) {
+			stageResults = new SyllabifierStageResults();
+			putExtension(SyllabifierStageResults.class, stageResults);
+			
+			stageResults.stages.put("<init>", new IPATranscript(phones).toString(true));
+		}
+		
 		for(StageType stage:def.getStage()) {
 			Stage currentStage = new Stage(stage);
 			
@@ -93,6 +114,10 @@ public class BasicSyllabifier implements Syllabifier {
 			} else {
 				currentStage.run(phones, scale);
 			}
+			
+			if(trackStages) {
+				stageResults.stages.put(stage.getName(), new IPATranscript(phones).toString(true));
+			}
 		}
 
 		for(IPAElement p:phones) {
@@ -105,6 +130,26 @@ public class BasicSyllabifier implements Syllabifier {
 	@Extension(IPAElement.class)
 	private class LastScType {
 		SyllableConstituentType lastType = SyllableConstituentType.UNKNOWN;
+	}
+
+	@Override
+	public Set<Class<?>> getExtensions() {
+		return extSupport.getExtensions();
+	}
+
+	@Override
+	public <T> T getExtension(Class<T> cap) {
+		return extSupport.getExtension(cap);
+	}
+
+	@Override
+	public <T> T putExtension(Class<T> cap, T impl) {
+		return extSupport.putExtension(cap, impl);
+	}
+
+	@Override
+	public <T> T removeExtension(Class<T> cap) {
+		return extSupport.removeExtension(cap);
 	}
 
 }
