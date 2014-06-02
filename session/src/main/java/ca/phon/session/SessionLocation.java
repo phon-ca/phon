ba0f862d267ae6f26d86186823d71dfa8bdfe871
Gregory Hedlund <ghedlund@mun.ca>
2014-06-02 09:47:38 -0230

    Moved SessionRange/SessionLocation classes to ca.phon.session package.

--- session/src/main/java/ca/phon/session/SessionLocation.java
+++ session/src/main/java/ca/phon/session/SessionLocation.java
@@ -1,71 +1,46 @@
-package ca.phon.session;
+/*
+ * Phon - An open source tool for research in phonology.
+ * Copyright (C) 2008 The Phon Project, Memorial University <http://phon.ling.mun.ca>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ca.phon.app.session.editor.view.find_and_replace;
+
+import ca.phon.util.Tuple;
 
 /**
- * Location within a {@link Session}.
- * Properties include record index,  tier name, group number
- * and character location within tier.
+ * Helper classes for holding current and start locations
  */
-public class SessionLocation {
-	
-	private Session session = null;
-	
-	private int recordIndex = 0;
-	
-	private int groupIndex = 0;
-	
-	private int characterPositionInGroup = 0;
+public class SessionLocation extends Tuple<Integer, RecordLocation> {
 
-	public SessionLocation() {
-		
+	public SessionLocation(Integer recIdx, RecordLocation recLoc) {
+		super(recIdx, recLoc);
 	}
 
-	public SessionLocation(Session session, int recordIndex, int groupIndex,
-			int characterPositionInGroup) {
-		super();
-		this.session = session;
-		this.recordIndex = recordIndex;
-		this.groupIndex = groupIndex;
-		this.characterPositionInGroup = characterPositionInGroup;
+	public Integer getRecordIndex() {
+		return super.getObj1();
 	}
 
-
-	public Session getSession() {
-		return session;
+	public void setRecordIndex(Integer idx) {
+		super.setObj1(idx);
 	}
 
-
-	public void setSession(Session session) {
-		this.session = session;
+	public RecordLocation getRecordLocation() {
+		return super.getObj2();
 	}
 
-
-	public int getRecordIndex() {
-		return recordIndex;
+	public void setRecordLocation(RecordLocation recLoc) {
+		super.setObj2(recLoc);
 	}
-
-
-	public void setRecordIndex(int recordIndex) {
-		this.recordIndex = recordIndex;
-	}
-
-
-	public int getGroupIndex() {
-		return groupIndex;
-	}
-
-
-	public void setGroupIndex(int groupIndex) {
-		this.groupIndex = groupIndex;
-	}
-
-
-	public int getCharacterPositionInGroup() {
-		return characterPositionInGroup;
-	}
-
-
-	public void setCharacterPositionInGroup(int characterPositionInGroup) {
-		this.characterPositionInGroup = characterPositionInGroup;
-	}
-	
 }
