ab4db36672f5affdf8cbc269f822c6a30eb53d5b
Gregory Hedlund <greg.hedlund@gmail.com>
2014-09-17 15:32:36 -0230

    Removed Recent Projects from File menu

--- app/src/main/java/ca/phon/app/menu/DefaultMenuFilter.java
+++ app/src/main/java/ca/phon/app/menu/DefaultMenuFilter.java
@@ -17,30 +17,17 @@
  */
 package ca.phon.app.menu;
 
-import java.awt.Toolkit;
 import java.awt.Window;
-import java.awt.event.KeyEvent;
 import java.util.logging.Logger;
 
-import javax.swing.Action;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.KeyStroke;
 import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
-import javax.swing.undo.UndoManager;
-import javax.swing.undo.UndoableEditSupport;
 
-import ca.phon.app.menu.edit.CopyCommand;
-import ca.phon.app.menu.edit.CutCommand;
 import ca.phon.app.menu.edit.EditMenuListener;
-import ca.phon.app.menu.edit.PasteCommand;
-import ca.phon.app.menu.edit.PreferencesCommand;
-import ca.phon.app.menu.edit.RedoCommand;
-import ca.phon.app.menu.edit.UndoCommand;
 import ca.phon.app.menu.file.ExitCommand;
-import ca.phon.app.menu.file.RecentProjectsMenuListener;
 import ca.phon.app.menu.file.WorkspaceCommand;
 import ca.phon.app.menu.help.HelpCommand;
 import ca.phon.app.menu.help.LogCommand;
@@ -51,9 +38,7 @@ import ca.phon.app.menu.tools.LanguageCodesCommand;
 import ca.phon.app.menu.window.OpenWindowsMenuListener;
 import ca.phon.app.prefs.PhonProperties;
 import ca.phon.app.workspace.WorkspaceDialog;
-import ca.phon.extensions.IExtendable;
 import ca.phon.plugin.IPluginMenuFilter;
-import ca.phon.plugin.PluginAction;
 import ca.phon.ui.CommonModuleFrame;
 import ca.phon.util.PrefHelper;
 
@@ -86,13 +71,6 @@ public class DefaultMenuFilter implements IPluginMenuFilter {
 		// start dialog item
 		final JMenuItem workspaceItem = new JMenuItem(new WorkspaceCommand());
 		fileMenu.add(workspaceItem);
-		
-		fileMenu.addSeparator();
-		
-		// recent menu
-		JMenu recentsMenu = new JMenu("Recent projects");
-		recentsMenu.addMenuListener(recentsMenuListener);
-		fileMenu.add(recentsMenu);
 		
 		// exit item
 		fileMenu.addSeparator();
@@ -131,39 +109,6 @@ public class DefaultMenuFilter implements IPluginMenuFilter {
 			menu.add(queryMenu);
 		}
 	}
-	
-//	private class SearchAction extends AbstractAction {
-//		
-//		private Project project;
-//		private QueryScript script;
-//		
-//		public SearchAction(Project project, QueryScript script) {
-//			super();
-//			this.project = project;
-//			this.script = script;
-//			
-//			final QueryName queryName = script.getExtension(QueryName.class);
-//			if(queryName != null) {
-//				String scriptName = queryName.getName();
-//				if(scriptName.indexOf('.') > 0) 
-//					scriptName = scriptName.substring(0, scriptName.lastIndexOf('.'));
-//				
-//				super.putValue(NAME, scriptName + "...");
-//			}
-//		}
-//
-//		@Override
-//		public void actionPerformed(ActionEvent e) {
-//			final QueryName queryName = script.getExtension(QueryName.class);
-//			final QueryEditorWindow scriptFrame = new QueryEditorWindow(queryName.getName(), project, script);
-//			scriptFrame.setWindowName(queryName.getName());
-////			scriptFrame.openFromFile(script.getAbsolutePath(), false);
-//			scriptFrame.pack();
-//			scriptFrame.setLocationByPlatform(true);
-//			scriptFrame.setVisible(true);
-//		}
-//		
-//	}
 	
 	/**
 	 * Add 'Tools' menu
@@ -229,11 +174,6 @@ public class DefaultMenuFilter implements IPluginMenuFilter {
 		
 		menu.add(helpMenu);
 	}
-	
-	/**
-	 * Recents menu generator
-	 */
-	private final MenuListener recentsMenuListener = new RecentProjectsMenuListener();
 	
 	/**
 	 * Query menu generator
