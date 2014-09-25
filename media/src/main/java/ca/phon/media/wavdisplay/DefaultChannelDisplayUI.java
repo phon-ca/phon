c2d23609d150babc0eea1b9c2c1c36d959cecee5
Gregory Hedlund <greg.hedlund@gmail.com>
2014-09-25 08:51:00 -0230

    Set STROKE_CONTROL to pure for waveform painting

--- media/src/main/java/ca/phon/media/wavdisplay/DefaultChannelDisplayUI.java
+++ media/src/main/java/ca/phon/media/wavdisplay/DefaultChannelDisplayUI.java
@@ -219,6 +219,9 @@ public class DefaultChannelDisplayUI extends ChannelDisplayUI {
 		
 		Graphics2D g2 = (Graphics2D)g;
 		
+		g2.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+		g2.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
+		
 		if(_buffer == null ||
 				(_buffer.getWidth() != _display.getWidth() || _buffer.getHeight() != _display.getHeight())) {
 			g.setColor(Color.black);
