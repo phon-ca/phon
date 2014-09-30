fe9641077f02b7dc009ad85f9b6e7f1a7f5a16df
Gregory Hedlund <greg.hedlund@gmail.com>
2014-09-30 10:04:43 -0230

    Setup location of derby database log

--- app/src/main/java/ca/phon/app/IPADictionaryInitHook.java
+++ app/src/main/java/ca/phon/app/IPADictionaryInitHook.java
@@ -1,5 +1,6 @@
 package ca.phon.app;
 
+import java.io.File;
 import java.util.logging.Logger;
 
 import ca.phon.app.hooks.PhonStartupHook;
@@ -7,11 +8,17 @@ import ca.phon.ipadictionary.impl.IPADatabaseManager;
 import ca.phon.plugin.IPluginExtensionFactory;
 import ca.phon.plugin.IPluginExtensionPoint;
 import ca.phon.plugin.PluginException;
+import ca.phon.util.PrefHelper;
 
 public class IPADictionaryInitHook implements PhonStartupHook, IPluginExtensionPoint<PhonStartupHook> {
 
 	private static final Logger LOGGER = Logger
 			.getLogger(IPADictionaryInitHook.class.getName());
+	
+	private static final String DERBY_LOG_PROP = "derby.stream.error.file";
+	
+	private static final String DERBY_LOG_LOCATION = 
+			PrefHelper.getUserDataFolder() + File.separator + "ipadb.log";
 	
 	@Override
 	public Class<?> getExtensionType() {
@@ -26,6 +33,8 @@ public class IPADictionaryInitHook implements PhonStartupHook, IPluginExtensionP
 	@Override
 	public void startup() throws PluginException {
 		LOGGER.info("Initializing IPA Dictionaries");
+		System.setProperty(DERBY_LOG_PROP,
+				PrefHelper.get(DERBY_LOG_PROP, DERBY_LOG_LOCATION));
 		IPADatabaseManager.getInstance();
 	}
 	
