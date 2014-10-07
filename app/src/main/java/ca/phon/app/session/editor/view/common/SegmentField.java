fb931d65b9338603200ac651a1179483461a36c0
Gregory Hedlund <greg.hedlund@gmail.com>
2014-10-06 21:42:28 -0230

    Added better validation to media segment field

--- app/src/main/java/ca/phon/app/session/editor/view/common/SegmentField.java
+++ app/src/main/java/ca/phon/app/session/editor/view/common/SegmentField.java
@@ -17,15 +17,32 @@
  */
 package ca.phon.app.session.editor.view.common;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.Shape;
 import java.text.ParseException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.swing.JFormattedTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultHighlighter;
+import javax.swing.text.Highlighter;
+import javax.swing.text.JTextComponent;
 import javax.swing.text.MaskFormatter;
 
+import ca.phon.session.MediaSegment;
+import ca.phon.session.MediaSegmentFormatter;
+
 public class SegmentField extends JFormattedTextField {
+	
+	private final Highlighter errHighlighter = new DefaultHighlighter();
 	
 	/**
 	 * 
@@ -38,8 +55,19 @@ public class SegmentField extends JFormattedTextField {
 	public SegmentField() {
 		super();
 		
+		errHighlighter.install(this);
 		setOpaque(false);
 		this.setFormatterFactory(new SegmentFormatterFactory());
+		
+		setBorder(new GroupFieldBorder());
+		
+		getDocument().addDocumentListener(docListener);
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		errHighlighter.paint(g);
 	}
 	
 	@Override
@@ -50,7 +78,51 @@ public class SegmentField extends JFormattedTextField {
 		
 		return retVal;
 	}
-
+	
+	public void validateText() {
+		errHighlighter.removeAllHighlights();
+		final MediaSegmentFormatter formatter = new MediaSegmentFormatter();
+		try {
+			final MediaSegment seg = formatter.parse(getText());
+			
+			if(seg.getStartValue() > seg.getEndValue()) {
+				((GroupFieldBorder)getBorder()).setShowWarningIcon(true);
+				setToolTipText("Start time is after end time");
+				try {
+					errHighlighter.addHighlight(0, getText().length(), errPainter);
+				} catch (BadLocationException e) {
+					LOGGER
+							.log(Level.SEVERE, e.getLocalizedMessage(), e);
+				}
+			} else {
+				((GroupFieldBorder)getBorder()).setShowWarningIcon(false);
+				setToolTipText(null);
+			}
+			
+		} catch (ParseException e) {
+			((GroupFieldBorder)getBorder()).setShowWarningIcon(true);
+			setToolTipText(e.getLocalizedMessage());
+		}
+	}
+	
+	private DocumentListener docListener = new DocumentListener() {
+		
+		@Override
+		public void removeUpdate(DocumentEvent e) {
+			
+		}
+		
+		@Override
+		public void insertUpdate(DocumentEvent e) {
+			validateText();
+		}
+		
+		@Override
+		public void changedUpdate(DocumentEvent e) {
+			
+		}
+	};
+	
 	/** Formatter factory */
 	private class SegmentFormatterFactory extends AbstractFormatterFactory {
 
@@ -68,4 +140,34 @@ public class SegmentField extends JFormattedTextField {
 
 	}
 	
+	private final Highlighter.HighlightPainter errPainter = new Highlighter.HighlightPainter() {
+		
+		@Override
+		public void paint(Graphics g, int p0, int p1, Shape bounds, JTextComponent c) {
+			final Graphics2D g2 = (Graphics2D)g;
+			
+			Rectangle b = bounds.getBounds();
+			try {
+				final Rectangle p0rect = c.modelToView(p0);
+				final Rectangle p1rect = c.modelToView(p1);
+				
+				b = new Rectangle(p0rect).union(p1rect);
+			} catch (BadLocationException e) {
+				
+			}
+			
+			g2.setColor(Color.red);
+			final float dash1[] = {1.0f};
+		    final BasicStroke dashed =
+		        new BasicStroke(1.0f,
+		                        BasicStroke.CAP_BUTT,
+		                        BasicStroke.JOIN_MITER,
+		                        1.0f, dash1, 0.0f);
+			g2.setStroke(dashed);
+			g2.drawLine(b.x, 
+					b.y + b.height - 1, 
+					b.x + b.width, 
+					b.y + b.height - 1);
+		}
+	};
 }
