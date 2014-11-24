455650e293f8a3c84fbced0afe96eec2505f33b4
Gregory Hedlund <greg.hedlund@gmail.com>
2014-11-24 09:31:15 -0330

    Added some swingx initializers to preloader

--- app/src/main/java/ca/phon/app/PreloaderStartupHook.java
+++ app/src/main/java/ca/phon/app/PreloaderStartupHook.java
@@ -8,6 +8,12 @@ import java.net.URL;
 import java.util.Enumeration;
 import java.util.logging.Logger;
 
+import javax.swing.JList;
+import javax.swing.SwingUtilities;
+
+import org.jdesktop.swingx.JXList;
+import org.jdesktop.swingx.JXTable;
+
 import ca.phon.app.hooks.PhonStartupHook;
 import ca.phon.plugin.IPluginExtensionFactory;
 import ca.phon.plugin.IPluginExtensionPoint;
@@ -56,6 +62,19 @@ public class PreloaderStartupHook implements PhonStartupHook, IPluginExtensionPo
 			}
 			in.close();
 		}
+		
+		// preload swingx renderers
+		final Runnable onEDT = new Runnable() {
+			
+			@Override
+			public void run() {
+				final JXTable tbl = new JXTable();
+				final JXList list = new JXList();
+			}
+			
+		};
+		SwingUtilities.invokeLater(onEDT);
+		
 	}
 	
 	private final IPluginExtensionFactory<PhonStartupHook> factory = new IPluginExtensionFactory<PhonStartupHook>() {
