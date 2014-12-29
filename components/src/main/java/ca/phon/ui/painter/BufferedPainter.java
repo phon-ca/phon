712e9660de449c0662383eaff63c981945a36577
Gregory Hedlund <greg.hedlund@gmail.com>
2014-12-29 10:39:30 -0330

    New buffered painter for automatically caching images.  Images are resized/scaled when the drawing surface size changes as required.

---
+++ components/src/main/java/ca/phon/ui/painter/BufferedPainter.java
@@ -0,0 +1,224 @@
+package ca.phon.ui.painter;
+
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.concurrent.atomic.AtomicReference;
+
+public abstract class BufferedPainter<T> implements Painter<T> {
+	
+	/**
+	 * This class will keep a reference to the size of the
+	 * previously rendered area.  When this changes, one
+	 * of two mode are supported.
+	 *
+	 */
+	public static enum ResizeMode {
+		/** Repaint buffer when the display area changes (default) */
+		REPAINT_ON_RESIZE,
+		/** Only repaint when height changes */
+		REPAINT_ON_RESIZE_Y,
+		/** Only repaint when width changes */
+		REPAINT_ON_RESIZE_X,
+		/** Scale the contents of the current buffer, useful when client data scales well */
+		SCALE_ON_RESIZE;
+	}
+	
+	/**
+	 * Repaint mode
+	 */
+	private ResizeMode resizeMode = ResizeMode.REPAINT_ON_RESIZE;
+	
+	/**
+	 * Flag to indicate the buffer needs to be repainted
+	 */
+	private volatile boolean repaintBuffer = true;
+	
+	/**
+	 * Atomic reference of the current buffer
+	 */
+	private final AtomicReference<BufferedImage> bufferRef = new AtomicReference<BufferedImage>();
+	
+	/**
+	 * Previous size of display area
+	 */
+	private final AtomicReference<Dimension> prevSizeRef = new AtomicReference<Dimension>(new Dimension());
+	
+	/**
+	 * Property support
+	 */
+	private final PropertyChangeSupport propSupport = new PropertyChangeSupport(this);
+	
+	public BufferedImage getBufferdImage() {
+		return bufferRef.get();
+	}
+	
+	public void setBufferedImage(BufferedImage img) {
+		bufferRef.set(img);
+	}
+	
+	public Dimension getPrevSize() {
+		return prevSizeRef.get();
+	}
+	
+	public void setPrevSize(Dimension size) {
+		prevSizeRef.set(size);
+	}
+	
+	public boolean isRepaintBuffer() {
+		return this.repaintBuffer;
+	}
+	
+	public void setRepaintBuffer(boolean repaintBuffer) {
+		this.repaintBuffer = repaintBuffer;
+	}
+	
+	public ResizeMode getResizeMode() {
+		return this.resizeMode;
+	}
+	
+	public void setResizeMode(ResizeMode mode) {
+		this.resizeMode = mode;
+	}
+	
+	/**
+	 * Paint the buffer.
+	 * 
+	 * @param obj
+	 * @param g2
+	 * @param bounds
+	 */
+	protected abstract void paintBuffer(T obj, Graphics2D g2, Rectangle2D bounds);
+
+	@Override
+	public void paint(T obj, Graphics2D g2, Rectangle2D bounds) {
+		final Dimension oldBounds = getPrevSize();
+		final Dimension newBounds = new Dimension((int)bounds.getWidth(), (int)bounds.getHeight());
+		if(oldBounds == null) {
+			setRepaintBuffer(true);
+		} else if(!oldBounds.equals(newBounds)) {
+			if(getResizeMode() == ResizeMode.REPAINT_ON_RESIZE) {
+				setRepaintBuffer(true);
+			} else if(getResizeMode() == ResizeMode.REPAINT_ON_RESIZE_X
+					&& oldBounds.getWidth() != newBounds.getWidth()) {
+				setRepaintBuffer(true);
+			} else if(getResizeMode() == ResizeMode.REPAINT_ON_RESIZE_Y
+					&& oldBounds.getHeight() != newBounds.getHeight()) {
+				setRepaintBuffer(true);
+			}
+		}
+		setPrevSize(newBounds);
+		
+		BufferedImage img = getBufferdImage();
+		if(img == null || isRepaintBuffer()) {
+			final BufferedImage buffer = new BufferedImage((int)newBounds.getWidth(), (int)newBounds.getHeight(), 
+					BufferedImage.TYPE_4BYTE_ABGR);
+			final Graphics2D bufferG2 = (Graphics2D)buffer.createGraphics();
+			paintBuffer(obj, bufferG2, new Rectangle2D.Double(0, 0, bounds.getWidth(), bounds.getHeight()));
+			setBufferedImage(buffer);
+			setRepaintBuffer(false);
+			img = getBufferdImage();
+		}
+		
+		// check for null in case something didn't work
+		// in the paintBuffer implementation
+		if(img != null) {
+			int imgHeight = img.getHeight();
+			int imgWidth = img.getWidth();
+			int surfaceHeight = (int)bounds.getHeight();
+			int surfaceWidth = (int)bounds.getWidth();
+			
+			final AffineTransform op = new AffineTransform();
+			op.translate(bounds.getX(), bounds.getY());
+			
+			if(getResizeMode() != ResizeMode.REPAINT_ON_RESIZE
+					&& (imgHeight != surfaceHeight || imgWidth != surfaceWidth)) {
+				// scale image
+				g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+				double xScale = (double)surfaceWidth / (double)imgWidth;
+				double yScale = (double)surfaceHeight / (double)imgHeight;
+				op.scale(xScale, yScale);
+			}
+			g2.drawImage(img, op, (ImageObserver) null);
+		}
+	}
+
+	/*
+	 * Property support
+	 */
+	public void addPropertyChangeListener(PropertyChangeListener listener) {
+		propSupport.addPropertyChangeListener(listener);
+	}
+
+	public void addPropertyChangeListener(String propertyName,
+			PropertyChangeListener listener) {
+		propSupport.addPropertyChangeListener(propertyName, listener);
+	}
+
+	public void fireIndexedPropertyChange(String propertyName, int index,
+			boolean oldValue, boolean newValue) {
+		propSupport.fireIndexedPropertyChange(propertyName, index, oldValue,
+				newValue);
+	}
+
+	public void fireIndexedPropertyChange(String propertyName, int index,
+			int oldValue, int newValue) {
+		propSupport.fireIndexedPropertyChange(propertyName, index, oldValue,
+				newValue);
+	}
+
+	public void fireIndexedPropertyChange(String propertyName, int index,
+			Object oldValue, Object newValue) {
+		propSupport.fireIndexedPropertyChange(propertyName, index, oldValue,
+				newValue);
+	}
+
+	public void firePropertyChange(PropertyChangeEvent evt) {
+		propSupport.firePropertyChange(evt);
+	}
+
+	public void firePropertyChange(String propertyName, boolean oldValue,
+			boolean newValue) {
+		propSupport.firePropertyChange(propertyName, oldValue, newValue);
+	}
+
+	public void firePropertyChange(String propertyName, int oldValue,
+			int newValue) {
+		propSupport.firePropertyChange(propertyName, oldValue, newValue);
+	}
+
+	public void firePropertyChange(String propertyName, Object oldValue,
+			Object newValue) {
+		propSupport.firePropertyChange(propertyName, oldValue, newValue);
+	}
+
+	public PropertyChangeListener[] getPropertyChangeListeners() {
+		return propSupport.getPropertyChangeListeners();
+	}
+
+	public PropertyChangeListener[] getPropertyChangeListeners(
+			String propertyName) {
+		return propSupport.getPropertyChangeListeners(propertyName);
+	}
+
+	public boolean hasListeners(String propertyName) {
+		return propSupport.hasListeners(propertyName);
+	}
+
+	public void removePropertyChangeListener(PropertyChangeListener listener) {
+		propSupport.removePropertyChangeListener(listener);
+	}
+
+	public void removePropertyChangeListener(String propertyName,
+			PropertyChangeListener listener) {
+		propSupport.removePropertyChangeListener(propertyName, listener);
+	}
+
+}
