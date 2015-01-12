648f963e2838cde4bc1dfadf6a0f3cf8d66bf48f
Gregory Hedlund <greg.hedlund@gmail.com>
2015-01-12 16:28:21 -0330

    Allow changing log props file location

--- app/src/main/java/ca/phon/app/log/LogManager.java
+++ app/src/main/java/ca/phon/app/log/LogManager.java
@@ -1,6 +1,9 @@
 package ca.phon.app.log;
 
 import java.io.IOException;
+import java.io.InputStream;
+
+import ca.phon.util.PrefHelper;
 
 /**
  * Method for setup and management of application logs.
@@ -12,7 +15,9 @@ public class LogManager {
 	 */
 	private final static LogManager _instance = new LogManager();
 	
-	private final static String PROPERTIES_FILE = "phonlog.properties";
+	public final static String PROPERTIES_FILE_LOCATION = LogManager.class.getName() + ".logProps";
+	
+	private final static String DEFAULT_PROPERTIES_FILE = "phonlog.properties";
 	
 	public static LogManager getInstance() {
 		return _instance;
@@ -21,10 +26,15 @@ public class LogManager {
 	private LogManager() {
 	}
 	
+	private InputStream getLogProps() {
+		return getClass().getClassLoader().getResourceAsStream(
+				PrefHelper.get(PROPERTIES_FILE_LOCATION, DEFAULT_PROPERTIES_FILE));
+	}
+	
 	public void setupLogging() {
 		final java.util.logging.LogManager manager = java.util.logging.LogManager.getLogManager();
 		try {
-			manager.readConfiguration(getClass().getResourceAsStream(PROPERTIES_FILE));
+			manager.readConfiguration(getLogProps());
 		} catch (SecurityException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
