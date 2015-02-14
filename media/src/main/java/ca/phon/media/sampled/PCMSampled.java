b460aca8b92bce5fc88c795cd018ed6393ddb152
Gregory Hedlund <greg.hedlund@gmail.com>
2015-02-14 11:48:06 -0330

    Improved performance of getWindowExtrema

--- media/src/main/java/ca/phon/media/sampled/PCMSampled.java
+++ media/src/main/java/ca/phon/media/sampled/PCMSampled.java
@@ -169,6 +169,42 @@ public class PCMSampled implements Sampled {
 		return retVal;
 	}
 	
+	protected int toSample(int idx, byte[] frames) {
+		int retVal = 0;
+		
+		final AudioFormat format = getAudioFileFormat().getFormat();
+		if(format.getEncoding() != Encoding.PCM_SIGNED &&
+				format.getEncoding() != Encoding.PCM_UNSIGNED)
+			throw new IllegalStateException("Unknown format");
+		
+		switch(format.getSampleSizeInBits()) {
+		case 8:
+			retVal = to8bitSample(idx, frames);
+			break;
+			
+		case 12:
+			retVal = to12bitSample(idx, frames);
+			break;
+			
+		case 16:
+			retVal = to16bitSample(idx, frames);
+			break;
+			
+		case 24:
+			retVal = to24bitSample(idx, frames);
+			break;
+			
+		case 32:
+			retVal = to32bitSample(idx, frames);
+			break;
+			
+		default:
+			break;
+		}
+		
+		return retVal;
+	}
+	
 	protected byte[] getBytes(int byteOffset, int len) {
 		byte[] retVal = new byte[len];
 		
@@ -183,16 +219,30 @@ public class PCMSampled implements Sampled {
 	}
 	
 	protected int get8bitSample(int byteOffset) {
-		final byte[] low = getBytes(byteOffset, 1);
-		final int sample = (low[0] & 0x00ff);
-		return sample;
+		final byte[] frame = getBytes(byteOffset, 1);
+		return to8bitSample(frame);
+	}
+	
+	protected int to8bitSample(byte[] frame) {
+		return to8bitSample(0, frame);
+	}
+	
+	protected int to8bitSample(int idx, byte[] frames) {
+		return (frames[idx] & 0xff);
 	}
 	
 	protected int get12bitSample(int byteOffset) {
 		byte sampleBytes[] = getBytes(byteOffset, 2);
-		
-		byte low = sampleBytes[0];
-		byte high = sampleBytes[1];
+		return to12bitSample(sampleBytes);
+	}
+	
+	protected int to12bitSample(byte[] frame) {
+		return to12bitSample(0, frame);
+	}
+	
+	protected int to12bitSample(int idx, byte[] frames) {
+		byte low = frames[idx];
+		byte high = frames[idx+1];
 
 		int sample = ((high << 8) & 0x0F00) + (low & 0x00FF);
 		return sample;
@@ -200,9 +250,16 @@ public class PCMSampled implements Sampled {
 	
 	protected int get16bitSample(int byteOffset) {
 		byte sampleBytes[] = getBytes(byteOffset, 2);
-		
-		byte low = sampleBytes[0];
-		byte high = sampleBytes[1];
+		return to16bitSamle(sampleBytes);
+	}
+	
+	protected int to16bitSamle(byte[] frame) {
+		return to16bitSample(0, frame);
+	}
+	
+	protected int to16bitSample(int idx, byte[] frames) {
+		byte low = frames[idx];
+		byte high = frames[idx+1];
 
 		int sample = (high << 8) + (low & 0x00ff);
 		return sample;
@@ -210,10 +267,17 @@ public class PCMSampled implements Sampled {
 
 	protected int get24bitSample(int byteOffset) {
 		byte sampleBytes[] = getBytes(byteOffset, 3);
-		
-		byte low = sampleBytes[0];
-		byte mid = sampleBytes[1];
-		byte high = sampleBytes[2];
+		return to24bitSample(sampleBytes);
+	}
+	
+	protected int to24bitSample(byte[] frame) {
+		return to24bitSample(0, frame);
+	}
+	
+	protected int to24bitSample(int idx, byte[] frames) {
+		byte low = frames[idx];
+		byte mid = frames[idx+1];
+		byte high = frames[idx+2];
 
 		int sample = (high << 16) + (mid << 8) + (low & 0x00ff);
 		return sample;
@@ -221,11 +285,18 @@ public class PCMSampled implements Sampled {
 	
 	protected int get32bitSample(int byteOffset) {
 		byte sampleBytes[] = getBytes(byteOffset, 4);
-		
-		byte low = sampleBytes[0];
-		byte lowmid = sampleBytes[1];
-		byte midhigh = sampleBytes[2];
-		byte high = sampleBytes[3];
+		return to32bitSample(sampleBytes);
+	}
+	
+	protected int to32bitSample(byte[] frame) {
+		return to32bitSample(0, frame);
+	}
+	
+	protected int to32bitSample(int idx, byte[] frames) {
+		byte low = frames[idx];
+		byte lowmid = frames[idx+1];
+		byte midhigh = frames[idx+2];
+		byte high = frames[idx+3];
 
 		int sample = ((high << 24) & 0xFF000000)
 				+ ((midhigh << 16) & 0x00FF0000)
@@ -305,8 +376,12 @@ public class PCMSampled implements Sampled {
 		long startIdx = sampleForTime(startTime);
 		long endIdx = sampleForTime(endTime);
 		
-		int offset = byteOffsetForFrame(startIdx);
-		int byteLength = (int)((endIdx-startIdx) * getAudioFileFormat().getFormat().getFrameSize());
+		return getBytes(startIdx, endIdx);
+	}
+	
+	public byte[] getBytes(long firstSample, long lastSample) {
+		int offset = byteOffsetForFrame(firstSample);
+		int byteLength = (int)((lastSample-firstSample) * getAudioFileFormat().getFormat().getFrameSize());
 		byte[] buffer = new byte[byteLength];
 		
 		try {
@@ -328,15 +403,19 @@ public class PCMSampled implements Sampled {
 		checkChannelBounds(channel);
 		checkSampleBounds(firstSample, lastSample);
 		
-		for(long i = firstSample; i <= lastSample && i < getNumberOfSamples(); i++) {
-			final double value = valueForSample(channel, i);
-			extrema[0] = (i == firstSample ? value :
+		byte[] frames = getBytes(firstSample, lastSample);
+		int frameSize = getAudioFileFormat().getFormat().getFrameSize();
+		int numFrames = frames.length / frameSize;
+		
+		for(int i = 0; i < numFrames; i++) {
+			final double value = toSample(i*frameSize, frames);
+			extrema[0] = (i == 0 ? value :
 					Math.min(extrema[0], value));
-			extrema[1] = (i == firstSample ? value: 
+			extrema[1] = (i == 0 ? value: 
 					Math.max(extrema[1], value));
 		}
 	}
-
+	
 	@Override
 	public void getWindowExtrema(int channel, float startTime, float endTime,
 			double[] extrema) {
