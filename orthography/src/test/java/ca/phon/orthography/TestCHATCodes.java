6e0f19b1910eac6b562955ad733fc8db5083cbe0
Gregory Hedlund <greg.hedlund@gmail.com>
2015-05-12 11:26:57 -0230

    Add TextCHATCodes

---
+++ orthography/src/test/java/ca/phon/orthography/TestCHATCodes.java
@@ -0,0 +1,48 @@
+package ca.phon.orthography;
+
+import java.text.ParseException;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class TestCHATCodes {
+	
+	/**
+	 * Test shortenings
+	 * 
+	 * E.g., hel<l>o
+	 * 
+	 * @throws ParseException
+	 */
+//	@Test
+//	public void testShortening() throws ParseException {
+//		final String testStr = "hel<l>o";
+//		Orthography ortho = testString(testStr);
+//		
+//		Assert.assertEquals(testStr, ortho.toString());
+//	}
+
+	@Test
+	public void testEvents() throws ParseException {
+		final String testStr = "(happening: test)";
+		Orthography ortho = testString(testStr);
+		
+		Assert.assertEquals(testStr, ortho.toString());
+	}
+	
+	/**
+	 * Prarse orthography, throw any errors
+	 * 
+	 * @param str
+	 * @return
+	 * @throws ParseException
+	 */
+	private Orthography testString(String str) throws ParseException {
+		return Orthography.parseOrthography(str);
+	}
+	
+}
