8eabb29463b72f7fdfab67d85fc0d37712cd74ec
Greg Hedlund <greg.hedlund@gmail.com>
2015-09-15 08:34:12 -0230

    Moved save status to separate label. Updated status bar with action to open corpus folder.

--- app/src/main/java/ca/phon/app/session/editor/SessionEditorStatusBar.java
+++ app/src/main/java/ca/phon/app/session/editor/SessionEditorStatusBar.java
@@ -1,6 +1,7 @@
 package ca.phon.app.session.editor;
 
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.GraphicsConfiguration;
@@ -8,9 +9,14 @@ import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import java.awt.Image;
 import java.awt.Transparency;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorConvertOp;
+import java.io.File;
 import java.lang.ref.WeakReference;
+import java.net.MalformedURLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.GrayFilter;
 import javax.swing.ImageIcon;
@@ -19,6 +25,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 import javax.swing.SwingConstants;
+import javax.swing.event.MouseInputAdapter;
 
 import org.jdesktop.swingx.JXStatusBar;
 import org.jdesktop.swingx.JXStatusBar.Constraint.ResizeBehavior;
@@ -36,6 +43,7 @@ import ca.phon.session.Session;
 import ca.phon.ui.fonts.FontPreferences;
 import ca.phon.util.ByteSize;
 import ca.phon.util.OSInfo;
+import ca.phon.util.OpenFileLauncher;
 import ca.phon.util.icons.IconManager;
 import ca.phon.util.icons.IconSize;
 import ca.phon.worker.PhonTask;
@@ -43,6 +51,8 @@ import ca.phon.worker.PhonTaskListener;
 import ca.phon.worker.PhonTask.TaskStatus;
 
 public class SessionEditorStatusBar extends JXStatusBar {
+	
+	private final static Logger LOGGER = Logger.getLogger(SessionEditorStatusBar.class.getName());
 
 	private static final long serialVersionUID = 286465072395883742L;
 
@@ -89,6 +99,8 @@ public class SessionEditorStatusBar extends JXStatusBar {
 	
 	private JLabel statusLabel;
 	
+	private JLabel sessionPathLabel;
+	
 	private ImageIcon modifiedIcon;
 	
 	private ImageIcon unmodifiedIcon;
@@ -117,42 +129,66 @@ public class SessionEditorStatusBar extends JXStatusBar {
 	}
 	
 	private void init() {
-		statusLabel = new JLabel(getEditor().getSession().getCorpus() + "/" + 
-				getEditor().getSession().getName());
-		statusLabel.setFont(FontPreferences.getSmallFont());
-		add(statusLabel, new JXStatusBar.Constraint(ResizeBehavior.FILL));
+		statusLabel = new JLabel();
 		
-		JComponent pbar = new JPanel(new FormLayout("pref", "10px"));
-		progressBar = new JProgressBar(SwingConstants.HORIZONTAL);
-		progressBar.setIndeterminate(false);
-		progressBar.setValue(0);
-		if(OSInfo.isMacOs()) {
-			// XXX make progress bar height smaller on mac
-			progressBar.setSize(80, 8);
-		}
-		pbar.add(progressBar, (new CellConstraints()).xy(1, 1));
-		
-		progressLabel = new JLabel() {
-			@Override
-			public void setText(String txt) {
-				super.setText(txt);
-				super.setToolTipText(txt);
-			}
-		};
-		progressLabel.setFont(FontPreferences.getSmallFont());
-		
-		add(progressLabel, new JXStatusBar.Constraint(200));
-		add(pbar, new JXStatusBar.Constraint(100));
-		
-		modifiedIcon = IconManager.getInstance().getIcon("actions/document-save-as", IconSize.XSMALL);
-		unmodifiedIcon = IconManager.getInstance().getDisabledIcon("actions/document-save-as", IconSize.XSMALL);
-		
+		modifiedIcon = IconManager.getInstance().getIcon("actions/document-save", IconSize.XSMALL);
+		unmodifiedIcon = IconManager.getInstance().getDisabledIcon("actions/document-save", IconSize.XSMALL);
 		if(getEditor().isModified()) {
 			statusLabel.setIcon(modifiedIcon);
 		} else {
 			statusLabel.setIcon(unmodifiedIcon);
 		}
 		statusLabel.setToolTipText(getStatusTooltipText());
+		add(statusLabel, new JXStatusBar.Constraint(IconSize.XSMALL.getWidth()));
+		
+		sessionPathLabel = new JLabel(getEditor().getSession().getCorpus() + "/" + 
+				getEditor().getSession().getName());
+		sessionPathLabel.setFont(FontPreferences.getSmallFont());
+		sessionPathLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		sessionPathLabel.setToolTipText(getEditor().getProject().getSessionPath(getEditor().getSession()));
+		sessionPathLabel.addMouseListener(new MouseInputAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				if(arg0.getClickCount() == 1) {
+					// XXX open session folder in file explorer
+					final File corpusFolder = 
+							new File(getEditor().getProject().getCorpusPath(getEditor().getSession().getCorpus()));
+					try {
+						OpenFileLauncher.openURL(corpusFolder.toURI().toURL());
+					} catch (MalformedURLException e) {
+						LOGGER.log(Level.SEVERE, e.getLocalizedMessage(), e);
+					}
+				}
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				sessionPathLabel.setForeground(Color.blue);
+			}
+
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+				sessionPathLabel.setForeground(Color.gray);
+			}
+			
+		});
+		sessionPathLabel.setForeground(Color.gray);
+		
+		add(sessionPathLabel, new JXStatusBar.Constraint(ResizeBehavior.FILL));
+		
+		JComponent pbar = new JPanel(new FormLayout("pref", 
+				(OSInfo.isMacOs() ? "10px" : "pref")));
+		progressBar = new JProgressBar(SwingConstants.HORIZONTAL);
+		progressBar.setIndeterminate(false);
+		progressBar.setValue(0);
+		pbar.add(progressBar, (new CellConstraints()).xy(1, 1));
+		
+		progressLabel = new JLabel();
+		progressLabel.setFont(FontPreferences.getSmallFont());
+		
+		add(progressLabel, new JXStatusBar.Constraint(200));
+		add(pbar, new JXStatusBar.Constraint(100));
 	}
 	
 	private String getStatusTooltipText() {
@@ -182,6 +218,10 @@ public class SessionEditorStatusBar extends JXStatusBar {
 	
 	public JLabel getStatusLabel() {
 		return this.statusLabel;
+	}
+	
+	public JLabel getSessionPathLabel() {
+		return this.sessionPathLabel;
 	}
 	
 	public JLabel getProgressLabel() {
