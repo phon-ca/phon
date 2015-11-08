30054e700f4f84a782d54c9b6b4f052f3c6546e6
Greg Hedlund <greg.hedlund@gmail.com>
2015-11-08 08:22:14 -0330

    Setup and serialization of sort node settings

--- app/src/main/java/ca/phon/app/opgraph/nodes/query/SortNodeSettingsPanel.java
+++ app/src/main/java/ca/phon/app/opgraph/nodes/query/SortNodeSettingsPanel.java
@@ -1,17 +1,57 @@
 package ca.phon.app.opgraph.nodes.query;
 
+import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ItemEvent;
+import java.util.Arrays;
+
+import javax.swing.Action;
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JSeparator;
+import javax.swing.SwingConstants;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.IconifyAction;
+
+import org.jdesktop.swingx.HorizontalLayout;
+import org.jdesktop.swingx.VerticalLayout;
+
+import com.jgoodies.forms.layout.FormLayout;
 
 import ca.phon.app.opgraph.nodes.query.SortNodeSettings.FeatureFamily;
+import ca.phon.app.opgraph.nodes.query.SortNodeSettings.SortColumn;
+import ca.phon.app.opgraph.nodes.query.SortNodeSettings.SortOrder;
 import ca.phon.app.opgraph.nodes.query.SortNodeSettings.SortType;
+import ca.phon.ui.action.PhonActionEvent;
+import ca.phon.ui.action.PhonUIAction;
+import ca.phon.ui.fonts.FontPreferences;
+import ca.phon.ui.layout.ButtonBarBuilder;
 import ca.phon.ui.text.PromptedTextField;
+import ca.phon.util.icons.IconManager;
+import ca.phon.util.icons.IconSize;
 
 public class SortNodeSettingsPanel extends JPanel {
 
 	private static final long serialVersionUID = 4289280424233502931L;
 	
 	private final SortNodeSettings settings;
+	
+	private JPanel sortByPanel;
+	
+	private JButton addSortButton;
 	
 	public SortNodeSettingsPanel(SortNodeSettings settings) {
 		super();
@@ -21,11 +61,99 @@ public class SortNodeSettingsPanel extends JPanel {
 	}
 	
 	private void init() {
+		setLayout(new VerticalLayout());
 		
+		SortColumn groupByColumn = getSettings().getGroupBy();
+		if(groupByColumn == null) {
+			groupByColumn = new SortColumn();
+			getSettings().setGroupBy(groupByColumn);
+		}
+		final SortColumnPanel groupByPanel = new SortColumnPanel(groupByColumn);
+		groupByPanel.setBorder(BorderFactory.createTitledBorder("Group by (optional)"));
+		add(groupByPanel);
+		
+		sortByPanel = new JPanel(new VerticalLayout());
+		
+		final ImageIcon icon = IconManager.getInstance().getIcon("actions/list-add", IconSize.SMALL);
+		final Action onAddAction = new PhonUIAction(this, "onAddColumn");
+		onAddAction.putValue(Action.NAME, "Add");
+		onAddAction.putValue(Action.SHORT_DESCRIPTION, "Add column to sort");
+		onAddAction.putValue(Action.SMALL_ICON, icon);
+		addSortButton = new JButton(onAddAction);
+		addSortButton.setVisible(settings.getSorting().size() < 3);
+		
+		int scIdx = 0;
+		for(SortColumn sc:settings.getSorting()) {
+			final SortColumnPanel scPanel = new SortColumnPanel(sc);
+			if(scIdx > 0 && scIdx < 2) {
+				final JComponent sep = createSeparator(scPanel);
+				sortByPanel.add(sep);
+			}
+			sortByPanel.add(scPanel);
+			++scIdx;
+		}
+		
+		final JPanel btmPanel = new JPanel(new VerticalLayout());
+		btmPanel.setBorder(BorderFactory.createTitledBorder("Sort by"));
+		btmPanel.add(sortByPanel);
+		btmPanel.add(ButtonBarBuilder.buildOkBar(addSortButton));
+		add(btmPanel);
+	}
+	
+	public void onAddColumn() {
+		if(settings.getSorting().size() == 3) return;
+		final SortColumn sc = new SortColumn();
+		settings.getSorting().add(sc);
+		final SortColumnPanel scPanel = new SortColumnPanel(sc);
+		final JComponent sep = createSeparator(scPanel);
+		sortByPanel.add(sep);
+		sortByPanel.add(scPanel);
+		addSortButton.setVisible(settings.getSorting().size() < 3);
+		revalidate();
+	}
+	
+	public void onRemoveColumn(SortColumnPanel scPanel) {
+		sortByPanel.remove(scPanel);
+		if(scPanel.getSeparator() != null)
+			sortByPanel.remove(scPanel.getSeparator());
+		settings.getSorting().remove(scPanel.getSortColumn());
+		addSortButton.setVisible(settings.getSorting().size() < 3);
+		revalidate();
 	}
 
 	public SortNodeSettings getSettings() {
 		return this.settings;
+	}
+	
+	private JComponent createSeparator(SortColumnPanel scPanel) {
+		final ImageIcon removeIcon =
+				IconManager.getInstance().getDisabledIcon("actions/list-remove", IconSize.SMALL);
+		final PhonUIAction removeAct = new PhonUIAction(this, "onRemoveColumn", scPanel);
+		removeAct.putValue(PhonUIAction.SHORT_DESCRIPTION, "Remove sort column");
+		removeAct.putValue(PhonUIAction.SMALL_ICON, removeIcon);
+		final JButton removeButton = new JButton(removeAct);
+		removeButton.setBorderPainted(false);
+		
+		final JPanel sep = new JPanel(new GridBagLayout());
+		final GridBagConstraints gbc = new GridBagConstraints();
+		gbc.anchor = GridBagConstraints.EAST;
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.gridwidth = 1;
+		gbc.gridheight = 1;
+		gbc.insets = new Insets(0, 0, 0, 0);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.weightx = 1.0;
+		sep.add(new JSeparator(SwingConstants.HORIZONTAL), gbc);
+		
+		gbc.weightx = 0.0;
+		++gbc.gridx;
+		sep.add(removeButton, gbc);
+		
+		scPanel.setSeparator(sep);
+		
+		return sep;
 	}
 	
 	private PromptedTextField createColumnField() {
@@ -35,12 +163,7 @@ public class SortNodeSettingsPanel extends JPanel {
 	}
 	
 	private JComboBox<SortType> createSortTypeBox() {
-		final SortType[] boxVals = new SortType[SortType.values().length + 1];
-		int idx = 0;
-		boxVals[idx++] = null;
-		for(SortType v:SortType.values()) boxVals[idx++] = v;
-		
-		final JComboBox<SortType> retVal = new JComboBox<>(boxVals);
+		final JComboBox<SortType> retVal = new JComboBox<>(SortType.values());
 		retVal.setSelectedItem(null);
 		return retVal;
 	}
@@ -56,4 +179,175 @@ public class SortNodeSettingsPanel extends JPanel {
 		return retVal;
 	}
 	
+	class SortColumnPanel extends JPanel {
+		private PromptedTextField columnField = createColumnField();
+		private JComboBox<SortType> typeBox = createSortTypeBox();
+		
+		// plain text options
+		private JPanel plainTextOptions = new JPanel();
+		private JRadioButton ascendingBox = new JRadioButton("Ascending");
+		private JRadioButton descendingBox = new JRadioButton("Descending");
+		
+		// feature options
+		private JPanel featureOptions = new JPanel();
+		private JComboBox<FeatureFamily> feature1Box = createFeatureBox();
+		private JComboBox<FeatureFamily> feature2Box = createFeatureBox();
+		private JComboBox<FeatureFamily> feature3Box = createFeatureBox();
+		
+		private final SortColumn sortColumn;
+		
+		private JComponent separator;
+		
+		public SortColumnPanel(SortColumn sortColumn) {
+			super();
+			
+			this.sortColumn = sortColumn;
+			init();
+		}
+		
+		private void init() {
+			setLayout(new GridBagLayout());
+			
+			final GridBagConstraints gbc = new GridBagConstraints();
+			gbc.anchor = GridBagConstraints.EAST;
+			gbc.fill = GridBagConstraints.HORIZONTAL;
+			gbc.gridwidth = 1;
+			gbc.gridheight = 1;
+			gbc.insets = new Insets(2, 2, 5, 2);
+			
+			columnField.setText(sortColumn.getColumn());
+			columnField.getDocument().addDocumentListener(new DocumentListener() {
+				
+				@Override
+				public void removeUpdate(DocumentEvent e) {
+					updateColumn();
+				}
+				
+				@Override
+				public void insertUpdate(DocumentEvent e) {
+					updateColumn();
+				}
+				
+				@Override
+				public void changedUpdate(DocumentEvent e) {
+					
+				}
+			});
+			gbc.gridy = 0;
+			gbc.gridx = 0;
+			gbc.weightx = 0.0;
+			add(new JLabel("Column:"), gbc);
+			
+			gbc.gridx++;
+			gbc.weightx = 1.0;
+			add(columnField, gbc);
+			
+			gbc.weightx = 0.0;
+			gbc.gridx = 0;
+			gbc.gridy++;
+			add(new JLabel("Sort type:"), gbc);
+			
+			typeBox.addItemListener( (e) -> {
+				plainTextOptions.setVisible(typeBox.getSelectedItem() == SortType.PLAIN);
+				featureOptions.setVisible(typeBox.getSelectedItem() == SortType.IPA);
+				sortColumn.setType((SortType)typeBox.getSelectedItem());
+			});
+			typeBox.setSelectedItem(sortColumn.getType());
+			gbc.gridx++;
+			gbc.weightx = 1.0;
+			add(typeBox, gbc);
+
+			final ButtonGroup grp = new ButtonGroup();
+			grp.add(ascendingBox);
+			grp.add(descendingBox);
+			ascendingBox.setSelected(sortColumn.getOrder() == SortOrder.ASCENDING);
+			descendingBox.setSelected(sortColumn.getOrder() == SortOrder.DESCENDING);
+			plainTextOptions.setLayout(new HorizontalLayout());
+			
+			final ChangeListener l = (e) -> {
+				if(ascendingBox.isSelected())
+					sortColumn.setOrder(SortOrder.ASCENDING);
+				else
+					sortColumn.setOrder(SortOrder.DESCENDING);
+			};
+			plainTextOptions.add(ascendingBox);
+			plainTextOptions.add(descendingBox);
+			ascendingBox.addChangeListener(l);
+			descendingBox.addChangeListener(l);
+			
+			gbc.gridx = 1;
+			gbc.gridy++;
+			gbc.insets = new Insets(0, 0, 0, 0);
+			add(plainTextOptions, gbc);
+			
+			featureOptions.setLayout(new VerticalLayout());
+			featureOptions.add(feature1Box);
+			featureOptions.add(feature2Box);
+			featureOptions.add(feature3Box);
+			
+			feature1Box.addItemListener( (e) -> {
+				feature2Box.setEnabled(feature1Box.getSelectedItem() != null);
+				feature3Box.setEnabled(feature1Box.getSelectedItem() != null 
+						&& feature2Box.getSelectedItem() != null);
+			});
+			feature2Box.addItemListener( (e) -> {
+				feature3Box.setEnabled(feature2Box.getSelectedItem() != null);
+			});
+			
+			feature1Box.addItemListener(this::updateFeatureOrder);
+			feature2Box.addItemListener(this::updateFeatureOrder);
+			feature3Box.addItemListener(this::updateFeatureOrder);
+			
+			feature2Box.setEnabled(false);
+			feature3Box.setEnabled(false);
+
+			final JComboBox<?>[] featureBoxes = new JComboBox[] { feature1Box, feature2Box, feature3Box };
+			final FeatureFamily[] featureOrder = sortColumn.getFeatureOrder();
+			for(int i = 0; i < featureOrder.length; i++) {
+				featureBoxes[i].setSelectedItem(featureOrder[i]);
+			}
+			gbc.gridy++;
+			add(featureOptions, gbc);
+			
+			add(new JSeparator(SwingConstants.HORIZONTAL));
+		}
+		
+		void setSeparator(JComponent sep) {
+			this.separator = sep;
+		}
+		
+		JComponent getSeparator() {
+			return this.separator;
+		}
+		
+		public SortColumn getSortColumn() {
+			return sortColumn;
+		}
+		
+		public void updateColumn() {
+			sortColumn.setColumn(columnField.getText().trim());
+		}
+		
+		public void updateFeatureOrder(ItemEvent e) {
+			int featureOrderLength = 0;
+			if(feature1Box.getSelectedItem() != null) {
+				featureOrderLength++;
+				
+				if(feature2Box.getSelectedItem() != null) {
+					featureOrderLength++;
+					
+					if(feature3Box.getSelectedItem() != null) {
+						featureOrderLength++;
+					}
+				}
+			}
+			
+			final FeatureFamily[] tempOrder = new FeatureFamily[3];
+			tempOrder[0] = (FeatureFamily)feature1Box.getSelectedItem();
+			tempOrder[1] = (FeatureFamily)feature2Box.getSelectedItem();
+			tempOrder[2] = (FeatureFamily)feature3Box.getSelectedItem();
+			final FeatureFamily[] finalOrder = Arrays.copyOf(tempOrder, featureOrderLength);
+			sortColumn.setFeatureOrder(finalOrder);
+		}
+	}
 }
--- app/src/main/java/ca/phon/app/opgraph/nodes/query/SortNodeSettingsXMLSerializer.java
+++ app/src/main/java/ca/phon/app/opgraph/nodes/query/SortNodeSettingsXMLSerializer.java
@@ -1,7 +1,10 @@
 package ca.phon.app.opgraph.nodes.query;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.print.attribute.standard.MediaSize.NA;
 import javax.xml.XMLConstants;
 import javax.xml.namespace.QName;
 
@@ -12,11 +15,10 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import ca.gedge.opgraph.OpGraph;
+import ca.gedge.opgraph.extensions.Extendable;
 import ca.gedge.opgraph.io.xml.XMLSerializer;
 import ca.gedge.opgraph.io.xml.XMLSerializerFactory;
 import ca.phon.app.opgraph.nodes.query.SortNodeSettings.FeatureFamily;
-import ca.phon.app.opgraph.nodes.query.SortNodeSettings.IPASortColumn;
-import ca.phon.app.opgraph.nodes.query.SortNodeSettings.PlainSortColumn;
 import ca.phon.app.opgraph.nodes.query.SortNodeSettings.SortColumn;
 import ca.phon.app.opgraph.nodes.query.SortNodeSettings.SortOrder;
 import ca.phon.app.opgraph.nodes.query.SortNodeSettings.SortType;
@@ -45,31 +47,35 @@ public class SortNodeSettingsXMLSerializer implements XMLSerializer {
 		final Element settingsEle = 
 				doc.createElementNS(NAMESPACE, PREFIX + ":" + QNAME.getLocalPart());
 		
-		if(settings.getGroupBy() != null && settings.getGroupBy().trim().length() > 0) {
+		if(settings.getGroupBy() != null && settings.getGroupBy().getColumn() != null &&
+				settings.getGroupBy().getColumn().trim().length() > 0) {
 			final Element groupByEle = 
-					doc.createElement("groupBy");
-			groupByEle.setTextContent(settings.getGroupBy());
+					doc.createElementNS(NAMESPACE, PREFIX + ":groupBy");
+			writeSortColumn(doc, groupByEle, settings.getGroupBy());
 			settingsEle.appendChild(groupByEle);
 		}
 		
 		for(SortColumn sc:settings.getSorting()) {
-			final Element scEle = doc.createElement("sortBy");
-			scEle.setAttribute("column", sc.getColumn());
-			scEle.setAttribute("type", sc.getType().toString().toLowerCase());
-			if(sc.getType() == SortType.PLAIN) {
-				scEle.setAttribute("order", 
-						((PlainSortColumn)sc).getOrder().toString().toLowerCase());
-			} else if(sc.getType() == SortType.IPA) {
-				final IPASortColumn ipaSc = (IPASortColumn)sc;
-				for(FeatureFamily family:ipaSc.getOrder()) {
-					final Element orderEle = doc.createElement("order");
-					orderEle.setTextContent(family.toString().toLowerCase());
-					scEle.appendChild(orderEle);
-				}
-			}
+			final Element scEle = doc.createElementNS(NAMESPACE, PREFIX + ":sortBy");
+			writeSortColumn(doc, scEle, sc);
 			settingsEle.appendChild(scEle);
 		}
 		parentElem.appendChild(settingsEle);
+	}
+	
+	private void writeSortColumn(Document doc, Element scEle, SortColumn sc) {
+		scEle.setAttribute("column", sc.getColumn());
+		scEle.setAttribute("type", sc.getType().toString().toLowerCase());
+		if(sc.getType() == SortType.PLAIN) {
+			scEle.setAttribute("order", 
+					sc.getOrder().toString().toLowerCase());
+		} else if(sc.getType() == SortType.IPA) {
+			for(FeatureFamily family:sc.getFeatureOrder()) {
+				final Element orderEle = doc.createElementNS(NAMESPACE, PREFIX + ":featureOrder");
+				orderEle.setTextContent(family.toString().toLowerCase());
+				scEle.appendChild(orderEle);
+			}
+		}
 	}
 
 	@Override
@@ -79,44 +85,59 @@ public class SortNodeSettingsXMLSerializer implements XMLSerializer {
 			throw new IOException("Incorrect element");
 		
 		final SortNodeSettings retVal = new SortNodeSettings();
-		
-		NodeList groupByList = elem.getElementsByTagName("groupBy");
-		if(groupByList.getLength() > 0) {
-			final Node groupByEle = groupByList.item(0);
-			final String groupByColumn = groupByEle.getNodeValue();
-			retVal.setGroupBy(groupByColumn);
-		}
-		
-		NodeList sortByList = elem.getElementsByTagName("sortBy");
-		for(int i = 0; i < sortByList.getLength(); i++) {
-			final Node sortByNode = sortByList.item(i);
-			final NamedNodeMap attrs = sortByNode.getAttributes();
-			final String type = 
-					attrs.getNamedItem("type").getNodeValue();
-			if(SortType.PLAIN.toString().equalsIgnoreCase(type)) {
-				final PlainSortColumn plainSortBy = new PlainSortColumn();
-				plainSortBy.setColumn(attrs.getNamedItem("column").getNodeValue());
-				if(attrs.getNamedItem("order") != null) {
-					final SortOrder order = 
-							SortOrder.fromString(attrs.getNamedItem("order").getNodeValue());
-					plainSortBy.setOrder(order);
-				}
-			} else if(SortType.IPA.toString().equalsIgnoreCase(type)) {
-				final IPASortColumn ipaSortBy = new IPASortColumn();
-				ipaSortBy.setColumn(attrs.getNamedItem("column").getNodeValue());
-				
-				NodeList orderList = sortByNode.getChildNodes();
-				FeatureFamily[] order = new FeatureFamily[orderList.getLength()];
-				for(int j = 0; j < orderList.getLength(); j++) {
-					final Node orderNode = orderList.item(j);
-					final FeatureFamily family = 
-							FeatureFamily.fromString(orderNode.getNodeValue());
-					order[j] = family;
-				}
-				ipaSortBy.setOrder(order);
+		retVal.getSorting().clear();
+		NodeList childNodes = elem.getChildNodes();
+		for(int i = 0; i < childNodes.getLength(); i++) {
+			final Node childNode = childNodes.item(i);
+			if(childNode.getNamespaceURI() != null && childNode.getNamespaceURI().equals(NAMESPACE) 
+					&& childNode.getLocalName().equals("groupBy")) {
+				final SortColumn groupBy = readSortColumn(childNode);
+				retVal.setGroupBy(groupBy);
+			} else if(childNode.getNamespaceURI() != null && childNode.getNamespaceURI().equals(NAMESPACE)
+					&& childNode.getLocalName().equals("sortBy")) {
+				final SortColumn sortBy = readSortColumn(childNode);
+				retVal.getSorting().add(sortBy);
 			}
 		}
 		
+		// attach to parent object
+		if(parent instanceof Extendable) {
+			((Extendable)parent).putExtension(SortNodeSettings.class, retVal);
+		}
+		
+		
+		return retVal;
+	}
+	
+	private SortColumn readSortColumn(Node sortColumnNode) {
+		SortColumn retVal = new SortColumn();
+		final NamedNodeMap attrs = sortColumnNode.getAttributes();
+		final String type = 
+				attrs.getNamedItem("type").getNodeValue();
+		if(SortType.PLAIN.toString().equalsIgnoreCase(type)) {
+			retVal.setColumn(attrs.getNamedItem("column").getNodeValue());
+			retVal.setType(SortType.PLAIN);
+			if(attrs.getNamedItem("order") != null) {
+				final SortOrder order = 
+						SortOrder.fromString(attrs.getNamedItem("order").getNodeValue());
+				retVal.setOrder(order);
+			}
+		} else if(SortType.IPA.toString().equalsIgnoreCase(type)) {
+			retVal.setColumn(attrs.getNamedItem("column").getNodeValue());
+			retVal.setType(SortType.IPA);
+			NodeList orderList = sortColumnNode.getChildNodes();
+			List<FeatureFamily> order = new ArrayList<>();
+			for(int j = 0; j < orderList.getLength(); j++) {
+				final Node orderNode = orderList.item(j);
+				if(orderNode.getNamespaceURI() != null && orderNode.getNamespaceURI().equals(NAMESPACE)
+						&& orderNode.getLocalName().equals("featureOrder")) {
+					final FeatureFamily family = 
+							FeatureFamily.fromString(orderNode.getTextContent());
+					order.add(family);
+				}
+			}
+			retVal.setFeatureOrder(order.toArray(new FeatureFamily[0]));
+		}
 		return retVal;
 	}
 
