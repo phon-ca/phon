2be7c598b8460095bba2e79e39dc86fdf7a9c20b
Greg Hedlund <greg.hedlund@gmail.com>
2015-11-10 08:56:58 -0330

    Accept model in arguments to entry point.

--- opgraph/src/main/java/ca/phon/opgraph/editor/OpgraphEditorEP.java
+++ opgraph/src/main/java/ca/phon/opgraph/editor/OpgraphEditorEP.java
@@ -8,6 +8,8 @@ import ca.phon.plugin.IPluginEntryPoint;
 
 public class OpgraphEditorEP implements IPluginEntryPoint {
 
+	public final static String OPGRAPH_MODEL_KEY = "model";
+	
 	public final static String EP_NAME = "OpgraphEditor";
 	
 	@Override
@@ -17,8 +19,11 @@ public class OpgraphEditorEP implements IPluginEntryPoint {
 
 	@Override
 	public void pluginStart(Map<String, Object> args) {
+		final OpgraphEditorModel model =
+				(args.containsKey(OPGRAPH_MODEL_KEY) ? (OpgraphEditorModel)args.get(OPGRAPH_MODEL_KEY) : 
+					new DefaultOpgraphEditorModel());
 		final Runnable onEDT = () -> {
-			final OpgraphEditor editor = new OpgraphEditor();
+			final OpgraphEditor editor = new OpgraphEditor(model);
 			editor.pack();
 			editor.setSize(1064, 768);
 			editor.setLocationByPlatform(true);
