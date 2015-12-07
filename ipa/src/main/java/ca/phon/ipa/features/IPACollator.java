d54356678c9fafb83b2dd9aaee1f03bc9aa7d7a0
Greg Hedlund <greg.hedlund@gmail.com>
2015-12-07 13:40:49 -0330

    IPA Collator now only uses consonants and vowels in rules

--- ipa/src/main/java/ca/phon/ipa/features/IPACollator.java
+++ ipa/src/main/java/ca/phon/ipa/features/IPACollator.java
@@ -2,6 +2,8 @@ package ca.phon.ipa.features;
 
 import java.text.ParseException;
 import java.text.RuleBasedCollator;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -13,7 +15,15 @@ public class IPACollator extends RuleBasedCollator {
 	private static String rules = null;
 	
 	private static final String createRules() {
-		if(rules == null) {final Set<Character> chars = FeatureMatrix.getInstance().getCharacterSet();
+		if(rules == null) {
+			final Set<Character> chars = new LinkedHashSet<>();
+			final Set<Character> allChars = FeatureMatrix.getInstance().getCharacterSet();
+			final FeatureSet cvSet = FeatureSet.fromArray(new String[]{"c", "v"});
+			for(Character c:allChars) {
+				if(FeatureMatrix.getInstance().getFeatureSet(c).intersect(cvSet).size() > 0) {
+					chars.add(c);
+				}
+			}
 			final StringBuffer buffer = new StringBuffer();
 			chars.forEach( (c) -> buffer.append(" < ").append(c) );
 			rules = buffer.toString();
