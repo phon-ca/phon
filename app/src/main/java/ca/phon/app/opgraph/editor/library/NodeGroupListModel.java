14792f00581e5a75f92517290c223568c8ebfd0b
Greg Hedlund <greg.hedlund@gmail.com>
2016-01-07 10:34:44 -0330

    Improved library cell rendering to allow for variable row heights in lists.  Fixed instantiation of query nodes from library view.

--- app/src/main/java/ca/phon/app/opgraph/editor/library/NodeGroupListModel.java
+++ app/src/main/java/ca/phon/app/opgraph/editor/library/NodeGroupListModel.java
@@ -1,7 +1,10 @@
 package ca.phon.app.opgraph.editor.library;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import javax.swing.AbstractListModel;
 import javax.swing.DefaultListModel;
 
 import ca.gedge.opgraph.library.NodeData;
@@ -9,9 +12,15 @@ import ca.gedge.opgraph.library.NodeData;
 /**
  *
  */
-public class NodeGroupListModel extends DefaultListModel<NodeData> {
+public class NodeGroupListModel extends AbstractListModel<NodeData> {
 
 	private static final long serialVersionUID = 7555354396826758697L;
+	
+	private final ArrayList<NodeData> data = new ArrayList<>();
+	
+	private List<NodeData> filteredData = null;
+	
+	private String filter = null;
 
 	public NodeGroupListModel() {
 		super();
@@ -19,8 +28,45 @@ public class NodeGroupListModel extends DefaultListModel<NodeData> {
 	
 	public NodeGroupListModel(List<NodeData> data) {
 		super();
-		
-		data.forEach( (e) -> super.addElement(e) );
+		this.data.addAll(data);
+	}
+
+	@Override
+	public int getSize() {
+		if(filteredData != null) {
+			return filteredData.size();
+		} else {
+			return data.size();
+		}
+	}
+
+	public String getFilter() {
+		return this.filter;
+	}
+	
+	public void setFilter(String filter) {
+		this.filter = filter;
+		if(this.filter == null) {
+			this.filteredData = null;
+		} else {
+			this.filteredData =
+				this.data.stream()
+					.filter( (e) -> e.name.toLowerCase().contains(filter.toLowerCase()) 
+							|| e.description.toLowerCase().contains(filter.toLowerCase()) )
+					.collect(Collectors.toList());
+		}
+		super.fireContentsChanged(this, 0, data.size());
+	}
+	
+	@Override
+	public NodeData getElementAt(int index) {
+		NodeData retVal = null;
+		if(filteredData != null) {
+			retVal = filteredData.get(index);
+		} else {
+			retVal = data.get(index);
+		}
+		return retVal;
 	}
 
 }
