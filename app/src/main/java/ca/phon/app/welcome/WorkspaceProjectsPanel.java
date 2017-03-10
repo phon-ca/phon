7f6698ad8f3f53503dc71d24f235e2bf60720208
Greg Hedlund <greg.hedlund@gmail.com>
2017-03-10 10:58:03 -0330

    Fixed names and titles in reports and analysis documents

--- app/src/main/java/ca/phon/app/welcome/WorkspaceProjectsPanel.java
+++ app/src/main/java/ca/phon/app/welcome/WorkspaceProjectsPanel.java
@@ -2,17 +2,17 @@
  * Phon - An open source tool for research in phonology.
  * Copyright (C) 2005 - 2016, Gregory Hedlund <ghedlund@mun.ca> and Yvan Rose <yrose@mun.ca>
  * Dept of Linguistics, Memorial University <https://phon.ca>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -65,23 +65,23 @@ import ca.phon.workspace.WorkspaceHistory;
  *
  */
 public class WorkspaceProjectsPanel extends JPanel {
-	
+
 	private final static Logger LOGGER = Logger.getLogger(WorkspaceProjectsPanel.class.getName());
-	
+
 	/* UI */
 	private MultiActionButton workspaceBtn;
-	
+
 	private FolderProjectList projectList;
-	
+
 	public WorkspaceProjectsPanel() {
 		super();
-		
+
 		final Preferences prefs = PrefHelper.getUserPreferences();
 		prefs.addPreferenceChangeListener(
 			evt -> {
 				if(evt.getKey().equals(Workspace.WORKSPACE_FOLDER)) {
 					final Runnable onEdt = new Runnable() {
-						
+
 						@Override
 						public void run() {
 							projectList.setFolder(Workspace.userWorkspaceFolder());
@@ -94,23 +94,23 @@ public class WorkspaceProjectsPanel extends JPanel {
 					else
 						SwingUtilities.invokeLater(onEdt);
 				}
-				
+
 			}
 		);
-		
+
 		init();
 	}
-	
+
 	public void refresh() {
 		projectList.refresh();
 	}
-	
+
 	private void init() {
 		setLayout(new BorderLayout());
-		
+
 		workspaceBtn = new MultiActionButton();
 		workspaceBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		
+
 		String infoTxt = "";
 		try {
 			BufferedReader r = new BufferedReader(new InputStreamReader(
@@ -124,48 +124,48 @@ public class WorkspaceProjectsPanel extends JPanel {
 			e.printStackTrace();
 		}
 		BgPainter bgPainter = new BgPainter();
-		
-		PhonUIAction selectHistoryAct = 
+
+		PhonUIAction selectHistoryAct =
 				new PhonUIAction(this, "onShowHistory");
 		selectHistoryAct.putValue(Action.NAME, "Select workspace");
 		selectHistoryAct.putValue(Action.SHORT_DESCRIPTION, "Select workspace folder from history");
-		
-		ImageIcon workspaceIcn = 
+
+		ImageIcon workspaceIcnL =
 				IconManager.getInstance().getSystemIconForPath(
-						Workspace.userWorkspaceFolder().getAbsolutePath(), "places/folder-workspace", IconSize.SMALL);
-		
-		workspaceBtn.setTopLabelText(WorkspaceTextStyler.toHeaderText("Set Workspace Folder"));
+						Workspace.userWorkspaceFolder().getAbsolutePath(), "places/folder-workspace", IconSize.MEDIUM);
+
+		workspaceBtn.setTopLabelText(WorkspaceTextStyler.toHeaderText("Select Workspace Folder"));
+		workspaceBtn.getTopLabel().setIcon(workspaceIcnL);
 		workspaceBtn.getTopLabel().setFont(FontPreferences.getTitleFont());
-		workspaceBtn.getBottomLabel().setIcon(workspaceIcn);
 		workspaceBtn.setBottomLabelText(Workspace.userWorkspaceFolder().getAbsolutePath());
 		workspaceBtn.setBackgroundPainter(bgPainter);
 		workspaceBtn.addMouseListener(bgPainter);
 		workspaceBtn.addAction(createShowWorkspaceAction());
 		workspaceBtn.setDefaultAction(selectHistoryAct);
-		
+
 		JPanel contentPanel = new JPanel();
 		contentPanel.setBackground(Color.white);
 		contentPanel.setOpaque(true);
 		contentPanel.setLayout(new BorderLayout());
-		
+
 		JXLabel infoLbl = new JXLabel(infoTxt);
 		contentPanel.add(infoLbl, BorderLayout.CENTER);
 		contentPanel.add(workspaceBtn, BorderLayout.SOUTH);
-		
+
 		projectList = new FolderProjectList();
-		
+
 		add(contentPanel, BorderLayout.NORTH);
 		add(projectList, BorderLayout.CENTER);
 	}
-	
+
 	public void onShowHistory(PhonActionEvent pae) {
 		final WorkspaceHistory history = new WorkspaceHistory();
-		
+
 		final JPopupMenu menu = new JPopupMenu();
 		final MenuBuilder builder = new MenuBuilder(menu);
-		
+
 		for(File workspaceFolder:history) {
-			ImageIcon workspaceIcn = 
+			ImageIcon workspaceIcn =
 					IconManager.getInstance().getSystemIconForPath(
 							workspaceFolder.getAbsolutePath(), "places/folder-workspace", IconSize.SMALL);
 			final PhonUIAction selectAction = new PhonUIAction(this, "onSelectFolder", workspaceFolder);
@@ -173,37 +173,37 @@ public class WorkspaceProjectsPanel extends JPanel {
 			selectAction.putValue(PhonUIAction.SMALL_ICON, workspaceIcn);
 			builder.addItem(".", selectAction);
 		}
-	
-		ImageIcon browseIcn = 
+
+		ImageIcon browseIcn =
 				IconManager.getInstance().getIcon("actions/document-open", IconSize.SMALL);
 		if(OSInfo.isMacOs()) {
 			ImageIcon finderIcon =
 					IconManager.getInstance().getSystemStockIcon(MacOSStockIcon.OpenFolderIcon, IconSize.SMALL);
 			if(finderIcon != null) browseIcn = finderIcon;
 		} else if(OSInfo.isWindows()) {
-			ImageIcon explorerIcon = 
+			ImageIcon explorerIcon =
 					IconManager.getInstance().getSystemStockIcon(WindowsStockIcon.FOLDEROPEN, IconSize.SMALL);
 			if(explorerIcon != null) browseIcn = explorerIcon;
 		}
 		builder.addSeparator(".", "browse");
-		
+
 		final Action showWorkspaceAct = createShowWorkspaceAction();
 		builder.addItem(".@browse", showWorkspaceAct);
-		
+
 		final SelectWorkspaceCommand cmd = new SelectWorkspaceCommand();
 		cmd.putValue(Action.NAME, "Browse for workspace folder...");
 		cmd.putValue(Action.SMALL_ICON, browseIcn);
 		builder.addItem(".", cmd);
-		
+
 		builder.addSeparator(".", "clear");
 		final PhonUIAction clearHistoryAct = new PhonUIAction(this, "onClearHistory");
 		clearHistoryAct.putValue(PhonUIAction.NAME, "Clear history");
 		clearHistoryAct.putValue(PhonUIAction.SHORT_DESCRIPTION, "Clear workspace history");
 		builder.addItem(".@clear", clearHistoryAct);
-		
+
 		menu.show(workspaceBtn, 0, workspaceBtn.getHeight());
 	}
-	
+
 	public void onShowWorkspace() {
 		try {
 			OpenFileLauncher.openURL(Workspace.userWorkspaceFolder().toURI().toURL());
@@ -211,83 +211,83 @@ public class WorkspaceProjectsPanel extends JPanel {
 			LOGGER.log(Level.SEVERE, e.getLocalizedMessage(), e);
 		}
 	}
-	
+
 	private Action createShowWorkspaceAction() {
 
 		String fsIconName = "apps/system-file-manager";
 		String fsName = "file system viewer";
-		
+
 		ImageIcon fsIcon = IconManager.getInstance().getIcon(fsIconName, IconSize.SMALL);
 		ImageIcon fsIconL = IconManager.getInstance().getIcon(fsIconName, IconSize.MEDIUM);
-		
+
 		if(OSInfo.isWindows()) {
 			fsName = "File Explorer";
-			
+
 			final String explorerPath = "C:\\Windows\\explorer.exe";
 			ImageIcon explorerIcon = IconManager.getInstance().getSystemIconForPath(explorerPath, IconSize.SMALL);
 			ImageIcon explorerIconL = IconManager.getInstance().getSystemIconForPath(explorerPath, IconSize.MEDIUM);
-			
+
 			if(explorerIcon != null)
 				fsIcon = explorerIcon;
 			if(explorerIconL != null)
 				fsIconL = explorerIconL;
 		} else {
 			fsName = "Finder";
-			
+
 			ImageIcon finderIcon = IconManager.getInstance().getSystemStockIcon(MacOSStockIcon.FinderIcon, IconSize.SMALL);
 			ImageIcon finderIconL = IconManager.getInstance().getSystemStockIcon(MacOSStockIcon.FinderIcon, IconSize.MEDIUM);
-			
+
 			if(finderIcon != null)
 				fsIcon = finderIcon;
 			if(finderIconL != null)
 				fsIconL = finderIconL;
 		}
-		
-		
+
+
 		final PhonUIAction act = new PhonUIAction(this, "onShowWorkspace");
 		act.putValue(PhonUIAction.NAME, "Show workspace");
 		act.putValue(PhonUIAction.SHORT_DESCRIPTION, "Show workspace folder");
 		act.putValue(PhonUIAction.SMALL_ICON, fsIcon);
 		act.putValue(PhonUIAction.LARGE_ICON_KEY, fsIconL);
-		
+
 		return act;
 	}
-	
+
 	public void onClearHistory() {
 		final WorkspaceHistory history = new WorkspaceHistory();
 		history.clearHistory();
 		history.addToHistory(Workspace.userWorkspaceFolder());
 	}
-	
+
 	public void onSelectFolder(File workspaceFolder) {
 		Workspace.setUserWorkspaceFolder(workspaceFolder);
 		projectList.setFolder(workspaceFolder);
 		workspaceBtn.setBottomLabelText(workspaceFolder.getAbsolutePath());
 	}
-	
+
 	public void onResetWorkspace(PhonActionEvent pae) {
 		final File defaultWorkspace = Workspace.defaultWorkspaceFolder();
 		Workspace.setUserWorkspaceFolder(defaultWorkspace);
-		
+
 		projectList.setFolder(defaultWorkspace);
 		workspaceBtn.setBottomLabelText(defaultWorkspace.getAbsolutePath());
 	}
-	
+
 	/**
 	 * Background painter
 	 */
 	private class BgPainter extends MouseInputAdapter implements Painter<MultiActionButton> {
 
 		private boolean useSelected = false;
-		
+
 		private Color origColor = Color.white;
-		
+
 		private Color selectedColor = new Color(0, 100, 200, 100);
-		
+
 		public BgPainter() {
-			
+
 		}
-		
+
 		@Override
 		public void paint(Graphics2D g, MultiActionButton object, int width,
 				int height) {
@@ -299,21 +299,21 @@ public class WorkspaceProjectsPanel extends JPanel {
 				GlowPathEffect effect = new GlowPathEffect();
 				effect.setRenderInsideShape(true);
 				effect.setBrushColor(selectedColor);
-				
+
 				// get rectangle
-				Rectangle2D.Double boundRect = 
+				Rectangle2D.Double boundRect =
 					new Rectangle2D.Double(0.0f, 0.0f, width, height);
-				
+
 				effect.apply(g, boundRect, 0, 0);
 			}
 		}
-		
+
 		@Override
 		public void mouseEntered(MouseEvent me) {
 			useSelected = true;
 			repaint();
 		}
-		
+
 		@Override
 		public void mouseExited(MouseEvent me) {
 			useSelected = false;
