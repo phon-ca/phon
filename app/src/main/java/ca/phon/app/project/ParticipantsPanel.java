4189bb73673a6d9f177b2a9acaf5d61a60a202f4
Greg Hedlund <greg.hedlund@gmail.com>
2017-03-24 11:25:17 -0230

    Allow selection/deselection of all participants.  Don't display history by default.

--- app/src/main/java/ca/phon/app/project/ParticipantsPanel.java
+++ app/src/main/java/ca/phon/app/project/ParticipantsPanel.java
@@ -34,53 +34,53 @@ import ca.phon.worker.PhonWorker;
  * listing {@link Participant}s indentified in checked sessions.  A basic
  * history, including # of records and age, is provided for each
  * session the {@link Participant} is found.
- * 
+ *
  */
 public class ParticipantsPanel extends JPanel {
-	
+
 	private static final long serialVersionUID = 427615208570981191L;
 
 	private SessionSelector sessionSelector = new SessionSelector();
 	private ParticipantSelector participantSelector = new ParticipantSelector();
 	private JXBusyLabel participantBusyLabel = new JXBusyLabel(new Dimension(16, 16));
-	
+
 	private Project project;
-	
+
 	public ParticipantsPanel() {
 		this(null);
 	}
-	
+
 	public ParticipantsPanel(Project project) {
 		super();
-		
+
 		this.project = project;
-		
+
 		init();
 	}
-	
+
 	private void init() {
 		setLayout(new GridBagLayout());
-		
+
 		sessionSelector = new SessionSelector();
-		
+
 		final TitledPanel sessionPanel = new TitledPanel("Sessions");
 		sessionPanel.getContentContainer().setLayout(new BorderLayout());
 		final JScrollPane sessionScroller = new JScrollPane(sessionSelector);
 		sessionSelector.getModel().addTreeModelListener(treeModelListener);
 		sessionPanel.getContentContainer().add(sessionScroller, BorderLayout.CENTER);
-		
+
 		final TitledPanel participantPanel = new TitledPanel("Participants");
 		participantSelector = new ParticipantSelector();
-		participantSelector.setRootVisible(false);
+//		participantSelector.setRootVisible(false);
 		final JScrollPane participantScroller = new JScrollPane(participantSelector);
 		participantPanel.getContentContainer().setLayout(new BorderLayout());
 		participantPanel.getContentContainer().add(participantScroller, BorderLayout.CENTER);
-		
+
 		participantBusyLabel = new JXBusyLabel(new Dimension(16, 16));
 		participantPanel.setLeftDecoration(participantBusyLabel);
-		
+
 		final GridBagConstraints gbc = new GridBagConstraints();
-		gbc.gridx = 0; 
+		gbc.gridx = 0;
 		gbc.gridy = 0;
 		gbc.weightx = 1.0;
 		gbc.weighty = 1.0;
@@ -90,77 +90,77 @@ public class ParticipantsPanel extends JPanel {
 		gbc.fill = GridBagConstraints.BOTH;
 		gbc.insets = new Insets(2, 2, 2, 2);
 		add(sessionPanel, gbc);
-		
+
 		++gbc.gridx;
 		add(participantPanel, gbc);
 	}
-	
+
 	private void updateParticipants() {
 		final Runnable onBg = () -> {
 			SwingUtilities.invokeLater(() -> participantBusyLabel.setBusy(true));
-			
+
 			final Collection<Participant> participantHistory =
 					project.getParticipants(sessionSelector.getSelectedSessions());
-			
+
 			final TristateCheckBoxTreeModel model = ParticipantSelector.createModel(participantHistory);
-			SwingUtilities.invokeLater( () -> { 
+			SwingUtilities.invokeLater( () -> {
 				participantSelector.setModel(model);
 				participantSelector.setCheckingStateForPath(new TreePath(participantSelector.getRoot()), TristateCheckBoxState.CHECKED);
 				participantBusyLabel.setBusy(false);
-				
-				participantSelector.expandAll(new TreePath(participantSelector.getRoot()));
+
+				participantSelector.expandPath(new TreePath(participantSelector.getRoot()));
 			});
 		};
 		PhonWorker.getInstance().invokeLater(onBg);
 	}
-	
+
 	public void setProject(Project project) {
 		this.project = project;
 		sessionSelector.setProject(project);
 		sessionSelector.getModel().addTreeModelListener(treeModelListener);
 		sessionSelector.revalidate();
 	}
-	
+
 	public Project getProject() {
 		return this.project;
 	}
-	
+
 	public SessionSelector getSessionSelector() {
 		return this.sessionSelector;
 	}
-	
+
 	public ParticipantSelector getParticipantSelector() {
 		return this.participantSelector;
 	}
-	
+
 	public Collection<SessionPath> getCheckedSessions() {
 		return sessionSelector.getSelectedSessions();
 	}
-	
+
 	public Collection<Participant> getCheckedParticipants() {
 		return participantSelector.getSelectedParticpants();
 	}
 
 	private final TreeModelListener treeModelListener = new TreeModelListener() {
-		
+
 		@Override
 		public void treeStructureChanged(TreeModelEvent e) {
 		}
-		
+
 		@Override
 		public void treeNodesRemoved(TreeModelEvent e) {
 		}
-		
+
 		@Override
 		public void treeNodesInserted(TreeModelEvent e) {
 		}
-		
+
 		@Override
 		public void treeNodesChanged(TreeModelEvent e) {
 			if(e instanceof TristateCheckBoxTreeModelEvent) {
 				updateParticipants();
 			}
 		}
-		
+
 	};
 }
