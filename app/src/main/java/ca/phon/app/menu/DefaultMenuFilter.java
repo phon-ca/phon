1271da17debfc1d8c5392bb5978d361982ebb780
Greg Hedlund <greg.hedlund@gmail.com>
2017-03-28 08:34:08 -0230

    Do not display macro menu

--- app/src/main/java/ca/phon/app/menu/DefaultMenuFilter.java
+++ app/src/main/java/ca/phon/app/menu/DefaultMenuFilter.java
@@ -2,17 +2,17 @@
  * Phon - An open source tool for research in phonology.
  * Copyright (C) 2005 - 2016, Gregory Hedlund <ghedlund@mun.ca> and Yvan Rose <yrose@mun.ca>
  * Dept of Linguistics, Memorial University <https://phon.ca>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -52,19 +52,19 @@ import ca.phon.util.PrefHelper;
 
 /**
  * Create the default menu for all Phon windows.
- * 
+ *
  */
 public class DefaultMenuFilter implements IPluginMenuFilter {
 
 	private final static Logger LOGGER = Logger
 			.getLogger(DefaultMenuFilter.class.getName());
-	
+
 	@Override
 	public void filterWindowMenu(Window owner, JMenuBar menu) {
 		addFileMenu(owner, menu);
 		addEditMenu(owner, menu);
 		addWorkspaceMenu(owner, menu);
-		addMacroMenu(owner, menu);
+//		addMacroMenu(owner, menu);
 		addQueryMenu(owner, menu);
 		addAnalysisMenu(owner, menu);
 		addToolsMenu(owner, menu);
@@ -72,44 +72,44 @@ public class DefaultMenuFilter implements IPluginMenuFilter {
 		addWindowMenu(owner, menu);
 		addHelpMenu(owner, menu);
 	}
-	
+
 	/**
 	 * Add 'File' menu
 	 */
 	protected void addFileMenu(Window owner, JMenuBar menu) {
 		JMenu fileMenu = new JMenu("File");
-		
+
 		fileMenu.add(new NewProjectCommand());
 		fileMenu.add(new OpenProjectCommand());
-		
+
 		final JMenu recentProjectsMenu = new JMenu("Recent Projects");
 		recentProjectsMenu.addMenuListener(new RecentProjectsMenuListener());
 		fileMenu.add(recentProjectsMenu);
-		
+
 		fileMenu.addSeparator();
-		
+
 		final JMenuItem exitItem = new JMenuItem(new ExitCommand());
 		fileMenu.add(exitItem);
-		
+
 		menu.add(fileMenu);
 	}
 
 	/**
 	 * Add 'Edit' menu
-	 * 
+	 *
 	 */
 	protected void addEditMenu(Window owner, JMenuBar menu) {
 		JMenu editMenu = new JMenu("Edit");
-		
+
 		final MenuListener editListener = new EditMenuListener(owner);
 		editMenu.addMenuListener(editListener);
-		
+
 		final MenuEvent me = new MenuEvent(editMenu);
 		editListener.menuSelected(me);
-		
+
 		menu.add(editMenu);
 	}
-	
+
 	/**
 	 * Add 'Query' menu
 	 */
@@ -120,14 +120,14 @@ public class DefaultMenuFilter implements IPluginMenuFilter {
 			final JMenu queryMenu  = new JMenu("Query");
 			final QueryMenuListener queryMenuListener = new QueryMenuListener();
 			queryMenu.addMenuListener(queryMenuListener);
-			
+
 			final MenuEvent me = new MenuEvent(queryMenu);
 			queryMenuListener.menuSelected(me);
-			
+
 			menu.add(queryMenu);
 		}
 	}
-	
+
 	/**
 	 * Add macro menu
 	 */
@@ -139,14 +139,14 @@ public class DefaultMenuFilter implements IPluginMenuFilter {
 			final JMenu macroMenu = new JMenu("Macro");
 			final MacroMenuListener macroMenuListener = new MacroMenuListener();
 			macroMenu.addMenuListener(macroMenuListener);
-			
+
 			final MenuEvent me = new MenuEvent(macroMenu);
 			macroMenuListener.menuSelected(me);
-			
+
 			menu.add(macroMenu);
 		}
 	}
-	
+
 	/**
 	 * Add 'Assessment' menu
 	 */
@@ -158,37 +158,37 @@ public class DefaultMenuFilter implements IPluginMenuFilter {
 			final JMenu assessmentMenu = new JMenu("Analysis");
 			final AnalysisMenuListener assessmentMenuListener = new AnalysisMenuListener();
 			assessmentMenu.addMenuListener(assessmentMenuListener);
-			
+
 			final MenuEvent me = new MenuEvent(assessmentMenu);
 			assessmentMenuListener.menuSelected(me);
-			
+
 			menu.add(assessmentMenu);
 		}
 	}
-	
+
 	/**
 	 * Add 'Tools' menu
 	 */
 	protected void addToolsMenu(Window owner, JMenuBar menu) {
 		JMenu toolsMenu = new JMenu("Tools");
-		
+
 		// ipa chart
 		final JMenuItem ipaItem = new JMenuItem(new IpaMapCommand());
 		toolsMenu.add(ipaItem);
-		
+
 		toolsMenu.addSeparator();
-		
+
 		// language codes
 		final JMenuItem langItem = new JMenuItem(new LanguageCodesCommand());
 		toolsMenu.add(langItem);
-		
+
 		if(PrefHelper.getBoolean(PhonProperties.DEBUG, false)) {
 			toolsMenu.add(new BasicSyllabifierTestCommand());
 		}
-		
+
 		menu.add(toolsMenu);
 	}
-	
+
 	/**
 	 * Add 'Window' menu
 	 */
@@ -196,14 +196,14 @@ public class DefaultMenuFilter implements IPluginMenuFilter {
 		JMenu windowMenu = new JMenu("Window");
 		final MenuListener openWindowMenuListener = new OpenWindowsMenuListener(owner);
 		windowMenu.addMenuListener(openWindowMenuListener);
-		
+
 		final MenuEvent me = new MenuEvent(windowMenu);
 		openWindowMenuListener.menuSelected(me);
-		
+
 		// keep on tops
 		menu.add(windowMenu);
 	}
-	
+
 	/**
 	 * Add 'Plugin' menu
 	 */
@@ -211,36 +211,36 @@ public class DefaultMenuFilter implements IPluginMenuFilter {
 		JMenu pluginMenu = new JMenu("Plugins");
 		menu.add(pluginMenu);
 	}
-	
+
 	/**
 	 * Add 'Help' menu
 	 */
 	protected void addHelpMenu(Window owner, JMenuBar menu) {
 		JMenu helpMenu = new JMenu("Help");
-		
+
 		final JMenuItem logItem = new JMenuItem(new LogCommand());
 		helpMenu.add(logItem);
-		
+
 		// about
 		final JMenuItem aboutItem = new JMenuItem(new HelpCommand());
 		helpMenu.add(aboutItem);
-		
+
 		menu.add(helpMenu);
 	}
-	
+
 	/**
 	 * Add 'Workspace' menu
 	 */
 	protected void addWorkspaceMenu(Window owner, JMenuBar menu) {
 		JMenu workspaceMenu = new JMenu("Workspace");
-		
+
 		workspaceMenu.add(new SelectWorkspaceCommand());
-		
+
 		final JMenu workspaceProjectsMenu = new JMenu("Workspace projects");
 		workspaceProjectsMenu.addMenuListener(new WorkspaceProjectsMenuListener());
 		workspaceMenu.add(workspaceProjectsMenu);
-		
+
 		menu.add(workspaceMenu);
 	}
-	
+
 }
