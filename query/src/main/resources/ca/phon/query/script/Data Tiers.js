69b9eab52f8b43b4135cab80564a116e172f5235
Greg Hedlund <greg.hedlund@gmail.com>
2017-04-05 12:41:04 -0230

    'Search by word' is now enabled by default

--- query/src/main/resources/ca/phon/query/script/Data Tiers.js
+++ query/src/main/resources/ca/phon/query/script/Data Tiers.js
@@ -9,7 +9,7 @@ var AlignedGroupFilter = require("lib/TierFilter").TierFilter;
 var WordFilter = require("lib/WordFilter").WordFilter;
 var SyllableFilter = require("lib/SyllableFilter").SyllableFilter;
 var ParticipantFilter = require("lib/ParticipantFilter").ParticipantFilter;
-	
+
 /********************************
  * Setup params
  *******************************/
@@ -35,37 +35,37 @@ function begin_search(s) {
 function setup_params(params) {
     filters.primary.param_setup(params);
     filters.primary.set_required(true);
-    
+
     filters.group.param_setup(params);
     var sep = new LabelScriptParam("", "<html><b>Aligned Group</b></html>");
     params.add(sep);
     filters.alignedGroup.param_setup(params);
-    
+
     filters.word.param_setup(params);
     var wordsep = new LabelScriptParam("", "<html><b>Aligned Word</b></html>");
     params.add(wordsep);
     filters.alignedWord.param_setup(params);
-    
+
     var alignedWordListener = new java.beans.PropertyChangeListener {
         propertyChange: function(e) {
             var enabled = e.source.getValue(e.source.paramId);
             filters.alignedWord.setEnabled(enabled);
-        }  
+        }
     };
     filters.word.searchByWordOpt.addPropertyChangeListener(alignedWordListener);
-    filters.alignedWord.setEnabled(false);
+    filters.alignedWord.setEnabled(filters.word.searchByWord);
 
     filters.speaker.param_setup(params);
 }
 
 function isGroupedTier(tierName) {
 	var retVal = true;
-	
-	var systemTierType = 
+
+	var systemTierType =
 		(SystemTierType.isSystemTier(tierName) ? SystemTierType.tierFromString(tierName) : null);
-	
+
 	retVal = (systemTierType != null ? systemTierType.isGrouped() : false);
-	
+
 	if(systemTierType == null) {
 		for(var i = 0; i < session.userTierCount; i++) {
 			var userTier = session.getUserTier(i);
@@ -75,18 +75,18 @@ function isGroupedTier(tierName) {
 			}
 		}
 	}
-	
+
 	return retVal;
 }
 
 function query_record(recordIndex, record) {
 	if(!filters.speaker.check_speaker(record.speaker)) return;
-    
+
     var searchTier = filters.primary.tier;
     var tierGrouped = isGroupedTier(searchTier);
 
     var groups = filters.group.getRequestedGroups(record);
-    
+
     if(!tierGrouped && groups.length > 0) {
     	// take only first group
     	var tmp = new Array();
@@ -98,13 +98,13 @@ function query_record(recordIndex, record) {
 	if(filters.alignedGroup.isUseFilter()) {
 	    groups = filters.alignedGroup.filter_groups(record, groups);
 	}
-	
+
 	for(var gIdx = 0; gIdx < groups.length; gIdx++) {
 		var group = groups[gIdx];
-		
+
 		if(filters.word.isUseFilter()) {
 			var words = filters.word.getRequestedWords(group, searchTier);
-			
+
 			for(var wIdx = 0; wIdx < words.length; wIdx++) {
 				var word = words[wIdx];
 				var checkWord = true;
@@ -114,7 +114,7 @@ function query_record(recordIndex, record) {
 				}
 				if(checkWord == true) {
 					var vals = filters.primary.patternFilter.find_pattern(word.getTier(searchTier));
-					
+
 					// get start index of word
 					var wordOffset = 0;
 					if(searchTier == "Orthography") {
@@ -126,60 +126,60 @@ function query_record(recordIndex, record) {
 					} else {
 						wordOffset = word.getTierWordLocation(searchTier);
 					}
-					
+
 					for(var i = 0; i < vals.length; i++) {
 						var v = vals[i];
-						
+
 						var result = factory.createResult();
 				        result.recordIndex = recordIndex;
 				        result.schema = "LINEAR";
-				        
+
 				        var startIdx = wordOffset + v.start;
 				        var endIdx = wordOffset + v.end;
-				        
+
 				        if(v.value instanceof IPATranscript) {
 				        	// we need to convert phone to string range
 				        	startIdx = word.getTier(searchTier).stringIndexOf(v.value);
 				        	endIdx = startIdx + v.value.toString().length();
 				        }
-				        
+
 				        var rv = factory.createResultValue();
 				        rv.tierName = searchTier;
 				        rv.groupIndex = group.groupIndex;
 				        rv.range = new Range(startIdx, endIdx, false);
 				        rv.data = v.value;
 				        result.addResultValue(rv);
-				        
+
 				        results.addResult(result);
 					}
 				}
 			}
 		} else {
 			var vals = filters.primary.patternFilter.find_pattern(group.getTier(searchTier));
-			
+
 			for(var i = 0; i < vals.length; i++) {
 				var v = vals[i];
-				
+
 				var result = factory.createResult();
 		        result.recordIndex = recordIndex;
 		        result.schema = "LINEAR";
-		        
+
 		        var startIdx = v.start;
 		        var endIdx = v.end;
-		        
+
 		        if(v.value instanceof IPATranscript) {
 		        	// we need to convert phone to string range
 		        	startIdx = group.getTier(searchTier).stringIndexOf(v.value);
 		        	endIdx = startIdx + v.value.toString().length();
 		        }
-		        
+
 		        var rv = factory.createResultValue();
 		        rv.tierName = searchTier;
 		        rv.groupIndex = group.groupIndex;
 		        rv.range = new Range(startIdx, endIdx, false);
 		        rv.data = v.value;
 		        result.addResultValue(rv);
-		        
+
 		        results.addResult(result);
 			}
 		}
--- query/src/main/resources/ca/phon/query/script/lib/WordFilter.js
+++ query/src/main/resources/ca/phon/query/script/lib/WordFilter.js
@@ -2,37 +2,37 @@
  * Phon - An open source tool for research in phonology.
  * Copyright (C) 2005 - 2015, Gregory Hedlund <ghedlund@mun.ca> and Yvan Rose <yrose@mun.ca>
  * Dept of Linguistics, Memorial University <https://phon.ca>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 /**
  * Code for specifying word by position.
  */
- 
+
 var PatternFilter = require("lib/PatternFilter").PatternFilter;
 
 exports.WordFilter = function(id) {
 
 	var sectionTitle = "Word Filter";
-	
+
 	var singletonParamInfo = {
 		"id" : id+".wSingleton",
 		"def": true,
 		"title": "Singleton words:",
 		"desc": "(groups with only one word)" };
 	this.wSingleton = singletonParamInfo.def;
-		
+
 	var posParamInfo = {
 		"id" : [ id+".wInitial", id+".wMedial", id+".wFinal" ],
 		"def": [ true, true, true ],
@@ -42,21 +42,21 @@ exports.WordFilter = function(id) {
 	this.wInitial = posParamInfo.def[0];
 	this.wMedial = posParamInfo.def[1];
 	this.wFinal = posParamInfo.def[2];
-		
+
 	var searchByWordParamInfo = {
 		"id": id+".searchByWord",
-		"def": false,
+		"def": true,
 		"title": "Search by word:",
 		"desc": ""
 	};
 	this.searchByWord = searchByWordParamInfo.def;
-		
+
 	this.searchByWordEnabled = true;
 	this.searchByWordOpt;
-	
+
 	var singletonGroupOpt;
 	var posGroupOpt;
-		
+
 	/**
 	 * Add params for the group, called automatically when needed.
 	 *
@@ -64,23 +64,23 @@ exports.WordFilter = function(id) {
 	 */
 	this.param_setup = function(params) {
 		// create a new section (collapsed by default)
-		var sep = new SeparatorScriptParam(sectionTitle, true); 
+		var sep = new SeparatorScriptParam(sectionTitle, true);
 		params.add(sep);
-		
+
 		// search singleton groups.
 		singletonGroupOpt = new BooleanScriptParam(
 			singletonParamInfo.id,
 			singletonParamInfo.desc,
 			singletonParamInfo.title,
 			singletonParamInfo.def);
-		
+
 		posGroupOpt = new MultiboolScriptParam(
 			posParamInfo.id,
 			posParamInfo.def,
 			posParamInfo.desc,
 			posParamInfo.title,
 			posParamInfo.numCols);
-		
+
 		if(this.searchByWordEnabled == true) {
 			var searchByWordOpt = new BooleanScriptParam(
 				searchByWordParamInfo.id,
@@ -88,7 +88,7 @@ exports.WordFilter = function(id) {
 				searchByWordParamInfo.title,
 				searchByWordParamInfo.def);
 			params.add(searchByWordOpt);
-			
+
 			var searchByWordListener = new java.beans.PropertyChangeListener {
 			    propertyChange: function(e) {
 			        var enabled = e.source.getValue(e.source.paramId) == true;
@@ -100,14 +100,14 @@ exports.WordFilter = function(id) {
 			var enabled = searchByWordOpt.getValue(searchByWordOpt.paramId) == true;
 	        singletonGroupOpt.setEnabled(enabled);
 	        posGroupOpt.setEnabled(enabled);
-	        
+
 			this.searchByWordOpt = searchByWordOpt;
 		}
-		
+
 		params.add(singletonGroupOpt);
 		params.add(posGroupOpt);
 	};
-	
+
 	/**
 	 * Returns a list of words for the given
 	 * group which match the criteria in the form.
@@ -116,20 +116,20 @@ exports.WordFilter = function(id) {
 	 *
 	 * @return array of object conforming to the following
 	 *  protocol
-	 * 
+	 *
 	 * {
 	 *   wordIndex: int,
 	 *   start: int,
 	 *   end: int,
-	 *   word: obj 
+	 *   word: obj
 	 * }
 	 */
 	this.getRequestedWords = function(group, tierName) {
 		var retVal = new java.util.ArrayList();
 		var retIdx = 0;
-		
+
 		tierName = tierName || (searchTier || "Orthography");
-		
+
 		var words = new Array();
 		var wordCount = group.getWordCount(tierName);
 		for(var wIndex = 0; wIndex < wordCount; wIndex++)
@@ -148,11 +148,10 @@ exports.WordFilter = function(id) {
 				retVal.add(word);
 			}
 		}
-	
+
 		return retVal.toArray();
 	};
-	
-	
+
 	this.isUseFilter = function() {
 	    if(this.searchByWordEnabled == true) {
 	        return this.searchByWord == true;
@@ -160,6 +159,6 @@ exports.WordFilter = function(id) {
 	        return true;
 	    }
 	};
-	
+
 
 };
