24824c8c03bbb3d04d002d8a0df423f1a153c9a9
Greg Hedlund <greg.hedlund@gmail.com>
2017-04-05 12:08:24 -0230

    Fix velocity template encoding.

--- app/src/main/java/ca/phon/app/opgraph/wizard/NodeWizardReportTemplate.java
+++ app/src/main/java/ca/phon/app/opgraph/wizard/NodeWizardReportTemplate.java
@@ -21,44 +21,44 @@ import org.apache.velocity.runtime.resource.loader.StringResourceLoader;
 import org.apache.velocity.runtime.resource.util.StringResourceRepository;
 
 public class NodeWizardReportTemplate {
-	
+
 	private final static Logger LOGGER = Logger.getLogger(NodeWizardReportTemplate.class.getName());
 
 	private String name;
-	
+
 	private String template;
-	
+
 	public NodeWizardReportTemplate() {
 		this(UUID.randomUUID().toString());
 	}
-	
+
 	public NodeWizardReportTemplate(String name) {
 		this(name, "");
 	}
-	
+
 	public NodeWizardReportTemplate(String name, String template) {
 		super();
-	
+
 		this.name = name;
 		this.template = template;
 	}
-	
+
 	public String getName() {
 		return this.name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public String getTemplate() {
 		return this.template;
 	}
-	
+
 	public void setTemplate(String template) {
 		this.template = template;
 	}
-	
+
 	public String merge(NodeWizardReportContext ctx) {
 		// TODO move velocity init somewhere else
 		final Properties p = new Properties();
@@ -67,20 +67,20 @@ public class NodeWizardReportTemplate {
 		} catch (IOException e) {
 			LOGGER.log(Level.SEVERE, e.getLocalizedMessage(), e);
 		}
-		
+
 		final VelocityEngine ve = new VelocityEngine();
 		ve.init(p);
-		
+
 		StringResourceRepository repository = StringResourceLoader.getRepository();
 		repository.putStringResource(getName(), getTemplate(), "UTF-8");
-		
+
 		// load template
-		final Template t = ve.getTemplate(getName());
+		final Template t = ve.getTemplate(getName(), "UTF-8");
 		final StringWriter sw = new StringWriter();
-		
+
 		t.merge(ctx.velocityContext(), sw);
-		
+
 		return sw.toString();
 	}
-	
+
 }
