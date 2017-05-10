fe18cb1174656c8c3a6a700a375d6b6c4fb539a8
Greg Hedlund <greg.hedlund@gmail.com>
2017-05-10 06:17:37 -0230

    Add script to query node output

--- app/src/main/java/ca/phon/app/opgraph/nodes/query/QueryNode.java
+++ app/src/main/java/ca/phon/app/opgraph/nodes/query/QueryNode.java
@@ -2,17 +2,17 @@
  * Phon - An open source tool for research in phonology.
  * Copyright (C) 2005 - 2017, Gregory Hedlund <ghedlund@mun.ca> and Yvan Rose <yrose@mun.ca>
  * Dept of Linguistics, Memorial University <https://phon.ca>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -60,40 +60,42 @@ import ca.phon.script.params.ScriptParameters;
 	showInLibrary=true
 )
 public class QueryNode extends OpNode implements NodeSettings {
-	
+
 	private QueryScript queryScript;
-	
+
 	private InputField projectInputField = new InputField("project", "Project", false, true, Project.class);
-	
+
 	private InputField sessionsInputField = new InputField("sessions", "List of sessions or query results", false,
 			true, new RecordContainerTypeValidator());
-	
+
 	private InputField paramsInputField = new InputField("parameters", "Map of query parameters, these will override query settings.",
 			true, true, Map.class);
-	
+
 	private OutputField projectOutputField = new OutputField("project", "Project", true, Project.class);
-	
+
 	private OutputField queryField = new OutputField("query",
 			"Query parameters", true, Query.class);
 
-	private OutputField resultsField = new OutputField("results", 
+	private OutputField resultsField = new OutputField("results",
 			"Result set, one per input session", true, ResultSet[].class);
-	
-	private OutputField scriptOutput = new OutputField("buffer",
+
+	private OutputField scriptOutputField = new OutputField("script", "Query script", true, QueryScript.class);
+
+	private OutputField bufferOutputField = new OutputField("buffer",
 			"Text output from query", true, String.class);
-	
+
 	private OutputField paramsOutputField = new OutputField("parameters",
 			"Parameters used for query, including those entered using the settings dialog", true, Map.class);
-	
+
 	public QueryNode() {
 		this(new QueryScript(""));
 	}
-	
+
 	public QueryNode(QueryScript queryScript) {
 		super();
-		
+
 		this.queryScript = queryScript;
-		
+
 		super.putField(projectInputField);
 		super.putField(sessionsInputField);
 		super.putField(paramsInputField);
@@ -101,8 +103,9 @@ public class QueryNode extends OpNode implements NodeSettings {
 		super.putField(queryField);
 		super.putField(paramsOutputField);
 		super.putField(resultsField);
-		super.putField(scriptOutput);
-		
+		super.putField(scriptOutputField);
+		super.putField(bufferOutputField);
+
 		putExtension(NodeSettings.class, this);
 	}
 
@@ -110,24 +113,24 @@ public class QueryNode extends OpNode implements NodeSettings {
 	public void operate(OpContext opCtx) throws ProcessingException {
 		final Project project = (Project)opCtx.get(projectInputField);
 		if(project == null) throw new ProcessingException(null, "No project available");
-		
+
 		final Object inputObj = opCtx.get(sessionsInputField);
 		if(inputObj == null) throw new ProcessingException(null, "No session information given");
-		
+
 		final QueryManager qm = QueryManager.getInstance();
 		final QueryFactory queryFactory = qm.createQueryFactory();
 		final Query query = queryFactory.createQuery(project);
-		
+
 		final QueryScript queryScript = getQueryScript();
 		final QueryScriptContext ctx = queryScript.getQueryContext();
-		
+
 		ScriptParameters scriptParams = new ScriptParameters();
 		try {
 			scriptParams = ctx.getScriptParameters(ctx.getEvaluatedScope());
 		} catch (PhonScriptException e) {
 			throw new ProcessingException(null, e);
 		}
-		
+
 		final Script qScript = query.getScript();
 		qScript.setSource(queryScript.getScript());
 		final Map<String, String> sparams = new HashMap<String, String>();
@@ -149,36 +152,36 @@ public class QueryNode extends OpNode implements NodeSettings {
 				if(inputParams != null && inputParams.containsKey(paramId)) {
 					sp.setValue(paramId, inputParams.get(paramId));
 				}
-				
+
 				if(paramId.endsWith("ignoreDiacritics")
 						&& opCtx.containsKey(NodeWizard.IGNORE_DIACRITICS_GLOBAL_OPTION)) {
 					sp.setValue(paramId, opCtx.get(NodeWizard.IGNORE_DIACRITICS_GLOBAL_OPTION));
 				}
-				
+
 				if(paramId.endsWith("caseSensitive")
 						&& opCtx.containsKey(NodeWizard.CASE_SENSITIVE_GLOBAL_OPTION)) {
 					sp.setValue(paramId, opCtx.get(NodeWizard.CASE_SENSITIVE_GLOBAL_OPTION));
 				}
-				
+
 				allParams.put(paramId, sp.getValue(paramId));
 			}
 		}
-		
+
 		// ensure query form validates (if available)
 		if(scriptPanel != null && !scriptPanel.checkParams()) {
 			throw new ProcessingException(null, "Invalid query settings");
 		}
-				
+
 		final List<RecordContainer> recordContainers =
 				RecordContainer.toRecordContainers(project, inputObj);
-		
+
 		final ResultSet[] results = new ResultSet[recordContainers.size()];
-		
+
 		final ByteArrayOutputStream bOut = new ByteArrayOutputStream();
 		final PrintStream scriptOutputStream = new PrintStream(bOut);
 		ctx.redirectStdErr(scriptOutputStream);
 		ctx.redirectStdOut(scriptOutputStream);
-		
+
 		int serial = 0;
 		for(RecordContainer rc:recordContainers) {
 			checkCanceled();
@@ -190,15 +193,16 @@ public class QueryNode extends OpNode implements NodeSettings {
 				throw new ProcessingException(null, e);
 			}
 		}
-		
+
 		// setup outputs
 		opCtx.put(projectOutputField, project);
 		opCtx.put(resultsField, results);
-		opCtx.put(scriptOutput, new String(bOut.toByteArray()));
+		opCtx.put(bufferOutputField, new String(bOut.toByteArray()));
 		opCtx.put(queryField, query);
 		opCtx.put(paramsOutputField, allParams);
+		opCtx.put(scriptOutputField, queryScript);
 	}
-	
+
 	public QueryScript getQueryScript() {
 		if(scriptPanel != null) {
 			return (QueryScript)scriptPanel.getScript();
@@ -223,7 +227,7 @@ public class QueryNode extends OpNode implements NodeSettings {
 
 	@Override
 	public void loadSettings(Properties arg0) {
-		
+
 	}
 
 }
