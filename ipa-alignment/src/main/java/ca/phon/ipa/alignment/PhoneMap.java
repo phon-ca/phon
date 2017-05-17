81907e3742c743b13f7af2f2040b96beaea6c7d3
Greg Hedlund <greg.hedlund@gmail.com>
2017-05-17 13:06:38 -0230

    Fix PhoneMap round-trip

--- ipa-alignment/src/main/java/ca/phon/ipa/alignment/PhoneMap.java
+++ ipa-alignment/src/main/java/ca/phon/ipa/alignment/PhoneMap.java
@@ -2,17 +2,17 @@
  * Phon - An open source tool for research in phonology.
  * Copyright (C) 2005 - 2016, Gregory Hedlund <ghedlund@mun.ca> and Yvan Rose <yrose@mun.ca>
  * Dept of Linguistics, Memorial University <https://phon.ca>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -33,28 +33,28 @@ import ca.phon.phonex.PhonexPattern;
 import ca.phon.util.PhonConstants;
 
 /**
- * 
+ *
  */
 public class PhoneMap extends AlignmentMap<IPAElement> implements IExtendable {
 	/** The target phonetic rep */
 	private IPATranscript targetRep;
 	/** The actual phonetic rep */
 	private IPATranscript actualRep;
-	
+
 	private final ExtensionSupport extSupport = new ExtensionSupport(PhoneMap.class, this);
-	
+
 	public PhoneMap() {
 		this(new IPATranscript(), new IPATranscript());
 	}
-	
+
 	/**
 	 * Constructor
 	 */
 	public PhoneMap(IPATranscript targetRep, IPATranscript actualRep) {
 		super();
-		
+
 		extSupport.initExtensions();
-		
+
 		setTargetRep(targetRep);
 		setActualRep(actualRep);
 	}
@@ -65,11 +65,11 @@ public class PhoneMap extends AlignmentMap<IPAElement> implements IExtendable {
 
 	public void setActualRep(IPATranscript actualRep) {
 		this.actualRep = actualRep;
-		
+
 		final AudiblePhoneVisitor visitor = new AudiblePhoneVisitor();
 		actualRep.accept(visitor);
-		
-		this.bottomElements = 
+
+		this.bottomElements =
 			visitor.getPhones().toArray(new IPAElement[0]);
 	}
 
@@ -79,11 +79,11 @@ public class PhoneMap extends AlignmentMap<IPAElement> implements IExtendable {
 
 	public void setTargetRep(IPATranscript targetRep) {
 		this.targetRep = targetRep;
-		
+
 		final AudiblePhoneVisitor visitor = new AudiblePhoneVisitor();
 		targetRep.accept(visitor);
-		
-		this.topElements = 
+
+		this.topElements =
 			visitor.getPhones().toArray(new IPAElement[0]);
 	}
 
@@ -106,30 +106,30 @@ public class PhoneMap extends AlignmentMap<IPAElement> implements IExtendable {
 	public <T> T removeExtension(Class<T> cap) {
 		return extSupport.removeExtension(cap);
 	}
-	
+
 	@Override
 	public String toString() {
 		return toString(false);
 	}
-	
+
 	public String toString(boolean includeScType) {
 		final IPAElement[] topEles = getTopElements();
 		final IPAElement[] btmEles = getBottomElements();
 		final Integer[] topAlign = getTopAlignment();
 		final Integer[] btmAlign = getBottomAlignment();
-		
+
 		if(btmAlign.length != getAlignmentLength() || topAlign.length != getAlignmentLength()) return "";
-		
+
 		final StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < getAlignmentLength(); i++) {
 			final int topEleIdx = (topAlign[i] == null ? -1 : topAlign[i]);
 			final int btmEleIdx = (btmAlign[i] == null ? -1 : btmAlign[i]);
-			
-			final IPAElement topEle = 
+
+			final IPAElement topEle =
 					(topEleIdx >= 0 && topEleIdx < topEles.length ? topEles[topEleIdx] : null);
-			final IPAElement btmEle = 
+			final IPAElement btmEle =
 					(btmEleIdx >= 0 && btmEleIdx < btmEles.length ? btmEles[btmEleIdx] : null);
-			
+
 			if(i > 0)
 				sb.append(',');
 			sb.append( (topEle != null ? topEle.getText() : PhonConstants.nullChar) );
@@ -142,18 +142,18 @@ public class PhoneMap extends AlignmentMap<IPAElement> implements IExtendable {
 				sb.append(":").append(btmEle.getScType().getIdChar());
 			}
 		}
-		
+
 		return sb.toString();
 	}
-	
+
 	/**
 	 * Utility method for building a PhoneMap object given a string provided by
 	 * the toString method. Original transcriptions are also required.
-	 * 
+	 *
 	 * @param target
 	 * @param actual
 	 * @param align the string representation of the alignment
-	 * 
+	 *
 	 * @throws IllegalArgumentException if the phones found in the align string
 	 *  do not match the give target/actual forms
 	 */
@@ -161,40 +161,40 @@ public class PhoneMap extends AlignmentMap<IPAElement> implements IExtendable {
 			String align) {
 		final String phonex = "(.+)\\u2194(.+)";
 		final PhonexPattern pattern = PhonexPattern.compile(phonex);
-		
+
 		final String[] alignments = align.split(",");
 		int alignLen = alignments.length;
-		
+
 		IPATranscript targetPhones = target.stripDiacritics().removePunctuation();
 		IPATranscript actualPhones = actual.stripDiacritics().removePunctuation();
-		
+
 		int topPhoneIdx = 0;
 		int btmPhoneIdx = 0;
 		Integer alignment[][] = new Integer[2][alignLen];
 		for(int i = 0; i < alignLen; i++) {
-			final IPATranscript alignedPhones = 
+			final IPATranscript alignedPhones =
 					(new IPATranscriptBuilder()).append(alignments[i]).toIPATranscript();
 			final PhonexMatcher matcher = pattern.matcher(alignedPhones);
 			if(matcher.matches()) {
-				final IPAElement g1 = new IPATranscript(matcher.group(1)).elementAt(0);
-				final IPAElement g2 = new IPATranscript(matcher.group(2)).elementAt(0);
-				
-				final IPAElement tele = targetPhones.elementAt(topPhoneIdx);
-				final IPAElement aele = actualPhones.elementAt(btmPhoneIdx);
-				
+				final IPAElement g1 = new IPATranscript(matcher.group(1)).stripDiacritics().removePunctuation().elementAt(0);
+				final IPAElement g2 = new IPATranscript(matcher.group(2)).stripDiacritics().removePunctuation().elementAt(0);
+
+				final IPAElement tele = (topPhoneIdx < targetPhones.length() ? targetPhones.elementAt(topPhoneIdx) : null);
+				final IPAElement aele = (btmPhoneIdx < actualPhones.length() ? actualPhones.elementAt(btmPhoneIdx) : null);
+
 				Integer alignCol[] = new Integer[2];
 				if(g1.getFeatureSet().hasFeature("null")) {
 					alignCol[0] = -1;
-				} else if(g1.toString().equals(tele.toString())) {
+				} else if(tele != null && g1.toString().equals(tele.toString())) {
 					alignCol[0] = topPhoneIdx++;
 				} else {
 					// transcripts don't match
 					throw new IllegalArgumentException(target + " is not the correct source transcript");
 				}
-				
+
 				if(g2.getFeatureSet().hasFeature("null")) {
 					alignCol[1] = -1;
-				} else if(g2.toString().equals(aele.toString())) {
+				} else if(aele != null && g2.toString().equals(aele.toString())) {
 					alignCol[1] = btmPhoneIdx++;
 				} else {
 					throw new IllegalArgumentException(actual + " is not the correct source transcript");
@@ -205,11 +205,11 @@ public class PhoneMap extends AlignmentMap<IPAElement> implements IExtendable {
 				throw new IllegalArgumentException(align);
 			}
 		}
-		
+
 		PhoneMap retVal = new PhoneMap(target, actual);
 		retVal.setTopAlignment(alignment[0]);
 		retVal.setBottomAlignment(alignment[1]);
 		return retVal;
 	}
-	
+
 }
