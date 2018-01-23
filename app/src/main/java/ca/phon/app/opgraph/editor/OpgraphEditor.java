5d593b75aef3b683f81bc87217fa9078ef82fa5e
Greg Hedlund <greg.hedlund@gmail.com>
2018-01-23 09:58:49 -0330

    toolbar not floatable

--- app/src/main/java/ca/phon/app/opgraph/editor/OpgraphEditor.java
+++ app/src/main/java/ca/phon/app/opgraph/editor/OpgraphEditor.java
@@ -2,17 +2,17 @@
  * Phon - An open source tool for research in phonology.
  * Copyright (C) 2005 - 2017, Gregory Hedlund <ghedlund@mun.ca> and Yvan Rose <yrose@mun.ca>
  * Dept of Linguistics, Memorial University <https://phon.ca>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -248,6 +248,7 @@ public class OpgraphEditor extends CommonModuleFrame {
 
 	protected void setupToolbar() {
 		toolBar.removeAll();
+		toolBar.setFloatable(false);
 
 		toolBar.add(new SaveAction(this));
 
