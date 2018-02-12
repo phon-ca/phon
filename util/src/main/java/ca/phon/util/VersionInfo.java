034929182cd4be8f1c8702fe655dba1c4fd969bf
Greg Hedlund <greg.hedlund@gmail.com>
2018-02-12 15:47:17 -0330

    Update version number

--- util/src/main/java/ca/phon/util/VersionInfo.java
+++ util/src/main/java/ca/phon/util/VersionInfo.java
@@ -2,17 +2,17 @@
  * Phon - An open source tool for research in phonology.
  * Copyright (C) 2005 - 2016, Gregory Hedlund <ghedlund@mun.ca> and Yvan Rose <yrose@mun.ca>
  * Dept of Linguistics, Memorial University <https://phon.ca>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -24,42 +24,42 @@ import java.util.logging.Logger;
 
 /**
  * Methods for determining the version number of the application.
- * 
+ *
  * Version information is read from the file: build.properties
  * which should be located in the root of the class loader.
  */
 public class VersionInfo {
-	
+
 	/**
-	 * Properties file 
+	 * Properties file
 	 */
 	private final static String VERSION_PROP_FILE = "/build.properties";
-	
+
 	/**
 	 * Major version prop name
 	 */
 	private final static String MAJOR_VERSION = "build.major";
-	
+
 	/**
 	 * Minor version prop name
 	 */
 	private final static String BUILD_MINOR = "build.minor";
-	
+
 	/**
 	 * Revision
 	 */
 	private final static String BUILD_REVISION = "build.revision";
-	
+
 	/**
-	 * Source Control revision 
+	 * Source Control revision
 	 */
 	private final static String BUILD_SCREVISION = "build.screvision";
-	
+
 	/**
 	 * The shared instance
 	 */
 	private static VersionInfo _instance;
-	
+
 	/**
 	 * Loaded properties
 	 */
@@ -72,13 +72,13 @@ public class VersionInfo {
 		if(_instance == null) {
 			_instance = new VersionInfo();
 			_instance.versionProps = new Properties();
-			
+
 			try {
 				InputStream is = VersionInfo.class.getResourceAsStream(VERSION_PROP_FILE);
 				if(is == null) {
-					_instance.versionProps.put(MAJOR_VERSION, "1");
-					_instance.versionProps.put(BUILD_MINOR, "5");
-					_instance.versionProps.put(BUILD_REVISION, "2");
+					_instance.versionProps.put(MAJOR_VERSION, "2");
+					_instance.versionProps.put(BUILD_MINOR, "2");
+					_instance.versionProps.put(BUILD_REVISION, "0");
 					_instance.versionProps.put(BUILD_SCREVISION, "XXXXXXXXXXXX");
 				} else {
 					_instance.versionProps.load(is);
@@ -90,57 +90,57 @@ public class VersionInfo {
 		}
 		return _instance;
 	}
-	
+
 	/**
-	 * Get major version 
+	 * Get major version
 	 */
 	public String getMajorVersion() {
 		return versionProps.getProperty(MAJOR_VERSION);
 	}
-	
+
 	/**
 	 * Get minor version
 	 */
 	public String getMinorVersion() {
 		return versionProps.getProperty(BUILD_MINOR);
 	}
-	
+
 	/**
 	 * Revision
 	 */
 	public String getRevision() {
 		return versionProps.getProperty(BUILD_REVISION);
 	}
-	
+
 	/**
 	 * Source control number
 	 */
 	public String getScRevision() {
 		return versionProps.getProperty(BUILD_SCREVISION);
 	}
-	
+
 	/**
 	 * Return version as:
-	 * 
+	 *
 	 * <major>.<minor>.<build>
 	 */
 	public String getVersion() {
-		String retVal = 
+		String retVal =
 			getMajorVersion() + "." +
-			getMinorVersion() + "." + 
+			getMinorVersion() + "." +
 			getRevision();
 		return retVal;
 	}
-	
+
 	/**
 	 * Return long version
-	 * 
+	 *
 	 * <major>.<minor>.<build> <revision>
 	 */
 	public String getLongVersion() {
-		String retVal = 
+		String retVal =
 			getVersion() + " " + getScRevision();
 		return retVal;
 	}
-	
+
 }
