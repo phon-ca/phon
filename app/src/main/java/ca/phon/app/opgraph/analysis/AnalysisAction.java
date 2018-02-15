7e3a2fcad05e75480f05d119dfcace6658e1213e
Greg Hedlund <greg.hedlund@gmail.com>
2018-02-15 08:14:20 -0330

    Execute SwingWorker properly

--- app/src/main/java/ca/phon/app/opgraph/analysis/AnalysisAction.java
+++ app/src/main/java/ca/phon/app/opgraph/analysis/AnalysisAction.java
@@ -2,22 +2,23 @@
  * Phon - An open source tool for research in phonology.
  * Copyright (C) 2005 - 2017, Gregory Hedlund <ghedlund@mun.ca> and Yvan Rose <yrose@mun.ca>
  * Dept of Linguistics, Memorial University <https://phon.ca>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package ca.phon.app.opgraph.analysis;
 
+import java.awt.Cursor;
 import java.awt.event.ActionEvent;
 import java.io.*;
 import java.net.*;
@@ -33,6 +34,7 @@ import ca.phon.app.opgraph.report.ReportAction;
 import ca.phon.opgraph.OpgraphIO;
 import ca.phon.project.Project;
 import ca.phon.session.SessionPath;
+import ca.phon.ui.CommonModuleFrame;
 import ca.phon.worker.PhonWorker;
 
 public class AnalysisAction extends HookableAction {
@@ -80,7 +82,7 @@ public class AnalysisAction extends HookableAction {
 	@Override
 	public void hookableActionPerformed(ActionEvent ae) {
 		final AnalysisWorker worker = new AnalysisWorker();
-		worker.run();
+		worker.execute();
 	}
 
 	private OpGraph loadAnalysis() throws IOException {
