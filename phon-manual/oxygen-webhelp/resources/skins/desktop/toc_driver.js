e60b989622d21d94b7fd2623833401a63de66b47
Greg Hedlund <greg.hedlund@gmail.com>
2018-03-21 20:42:01 -0230

    Updated manual

--- phon-manual/oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ phon-manual/oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180314025654", "oxygen-webhelp/search/index-1.js?uniqueId=20180314025654", "oxygen-webhelp/search/index-2.js?uniqueId=20180314025654", "oxygen-webhelp/search/index-3.js?uniqueId=20180314025654"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180321083739", "oxygen-webhelp/search/index-1.js?uniqueId=20180321083739", "oxygen-webhelp/search/index-2.js?uniqueId=20180321083739", "oxygen-webhelp/search/index-3.js?uniqueId=20180321083739"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
