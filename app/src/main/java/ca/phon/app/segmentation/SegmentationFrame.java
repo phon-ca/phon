17cd2bf1d04e840545cd5a3883e67bb68ee4569e
Greg Hedlund <greg.hedlund@gmail.com>
2018-09-24 09:30:20 -0230

    Added segmentation frame class

---
+++ app/src/main/java/ca/phon/app/segmentation/SegmentationFrame.java
@@ -0,0 +1,21 @@
+package ca.phon.app.segmentation;
+
+import ca.phon.media.player.PhonMediaPlayer;
+import ca.phon.media.sampled.PCMSegmentView;
+import ca.phon.ui.CommonModuleFrame;
+
+/**
+ * Window for media segmentation.
+ *
+ */
+public class SegmentationFrame extends CommonModuleFrame {
+
+	private String mediaLocation;
+	
+	private PhonMediaPlayer mediaPlayer;
+	
+	private PCMSegmentView segmentView;
+	
+	
+	
+}
