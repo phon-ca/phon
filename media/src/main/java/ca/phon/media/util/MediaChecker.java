40004b16195837613625f5005d8546248d5ebb8d
Greg Hedlund <greg.hedlund@gmail.com>
2020-03-02 07:31:59 -0330

    Add media checker for wav files

---
+++ media/src/main/java/ca/phon/media/util/MediaChecker.java
@@ -0,0 +1,77 @@
+package ca.phon.media.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import ca.phon.media.sampled.PCMSampled;
+import ca.phon.ui.nativedialogs.OSInfo;
+
+/**
+ * Check wav media to see if it can be loaded by
+ * the java audio system.
+ * 
+ * 
+ */
+public class MediaChecker {
+	
+	public static boolean checkMediaFile(String mediaFile) {
+		String className = MediaChecker.class.getName();
+		
+		final String javaHome = System.getProperty("java.home");
+		final String javaBin = javaHome + File.separator + "bin" + File.separator + "java" + 
+				(OSInfo.isWindows() ? ".exe" : "");
+		final String cp = System.getProperty("java.class.path");
+		final String libPath = System.getProperty("java.library.path");
+		
+		List<String> fullCmd = new ArrayList<String>();
+		String[] cmd = {
+				javaBin,
+				"-cp", cp,
+				"-Djava.library.path=" + libPath
+		};
+		fullCmd.addAll(Arrays.asList(cmd));
+		fullCmd.add(className);
+		fullCmd.add(mediaFile);
+		
+		// Fail if process exits 
+		// with state other than 0 or if process takes
+		// more than 1000ms to complete (considered a hang.)
+		ProcessBuilder pb = new ProcessBuilder(fullCmd);
+		try {
+			Process p = pb.start();
+			p.waitFor(1000L, TimeUnit.MILLISECONDS);
+			int exitValue = -1;
+			try {
+				exitValue = p.exitValue();
+			} catch (IllegalThreadStateException e) {
+				// try to terminate process
+				p.destroyForcibly();
+			} 
+			return (exitValue == 0);
+		} catch (IOException | InterruptedException e) {
+			return false;
+		}
+	}
+
+	public static void main(String[] args) {
+		if(args.length != 1) {
+			System.err.println("Usage 'java ca.phon.media.MediaChecker <file>'");
+			System.exit(1);
+		}
+		
+		try {
+			PCMSampled sample = new PCMSampled(new File(args[0]));
+			System.out.println(String.format("%s %fs OK", args[0], sample.getLength()));
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(2);
+		}
+		
+		System.exit(0);
+	}
+	
+}
