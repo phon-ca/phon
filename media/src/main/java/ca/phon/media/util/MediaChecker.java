fe71951b8091d96f669a92edb31c1f2dd55b14ab
Greg Hedlund <greg.hedlund@gmail.com>
2020-03-04 19:01:17 -0330

    Fixed media check on windows

--- media/src/main/java/ca/phon/media/util/MediaChecker.java
+++ media/src/main/java/ca/phon/media/util/MediaChecker.java
@@ -1,15 +1,21 @@
 package ca.phon.media.util;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import ca.phon.media.LongSound;
 import ca.phon.media.sampled.PCMSampled;
 import ca.phon.ui.nativedialogs.OSInfo;
+import ca.phon.worker.PhonWorker;
 
 /**
  * Check wav media to see if it can be loaded by
@@ -40,18 +46,30 @@ public class MediaChecker {
 		
 		// Fail if process exits 
 		// with state other than 0 or if process takes
-		// more than 1000ms to complete (considered a hang.)
+		// more than 2000ms to complete (considered a hang.)
 		ProcessBuilder pb = new ProcessBuilder(fullCmd);
+		pb.redirectErrorStream(true);
 		try {
 			Process p = pb.start();
-			int exitValue = -1;
-			if(!p.waitFor(1000L, TimeUnit.MILLISECONDS)) {
-				p.destroyForcibly();
-			} else {
-				exitValue = p.exitValue();
+			Timer timer = new Timer();
+			timer.schedule(new TimerTask() {
+				@Override
+				public void run() {
+					p.destroyForcibly();
+				}
+			}, 2000L);
+			
+			try (BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()))) {
+				String line;
+				while ((line = reader.readLine()) != null)
+				    System.err.println("mediacheck: " + line);
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-			return (exitValue == 0);
-		} catch (IOException | InterruptedException e) {
+			timer.cancel();
+			
+			return (p.exitValue() == 0);
+		} catch (IOException | IllegalThreadStateException e) {
 			return false;
 		}
 	}
@@ -63,8 +81,9 @@ public class MediaChecker {
 		}
 		
 		try {
+			System.err.println(String.format("Opening file %s", args[0]));
 			LongSound ls = LongSound.fromFile(new File(args[0]));
-			System.out.println(String.format("%s %fs OK", args[0], ls.length()));
+			System.err.println(String.format("%s %fs OK", args[0], ls.length()));
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.exit(2);
