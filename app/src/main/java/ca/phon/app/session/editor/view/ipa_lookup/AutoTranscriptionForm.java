869cdb4bfbf16fe0e1df963872e16ae520c37ce1
Greg Hedlund <greg.hedlund@gmail.com>
2020-03-24 13:52:20 -0230

    Comparator is no longer case sensitive

--- app/src/main/java/ca/phon/app/session/editor/view/ipa_lookup/AutoTranscriptionForm.java
+++ app/src/main/java/ca/phon/app/session/editor/view/ipa_lookup/AutoTranscriptionForm.java
@@ -153,7 +153,7 @@ public class AutoTranscriptionForm extends JPanel {
 
 		@Override
 		public int compare(Syllabifier o1, Syllabifier o2) {
-			return o1.toString().compareTo(o2.toString());
+			return o1.toString().toLowerCase().compareTo(o2.toString().toLowerCase());
 		}
 
 	}
