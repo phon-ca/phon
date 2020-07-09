fc454bb7149bdad4a7fb86b92e9c6ec45e9bbe35
Greg Hedlund <greg.hedlund@gmail.com>
2020-07-09 11:11:56 -0230

    Add entry point for debugger

--- app/src/main/java/ca/phon/app/phonex/PhonexDebuggerEP.java
+++ app/src/main/java/ca/phon/app/phonex/PhonexDebuggerEP.java
@@ -1,11 +1,16 @@
 package ca.phon.app.phonex;
 
+import java.awt.BorderLayout;
 import java.util.Map;
+
+import javax.swing.JMenuBar;
 
 import ca.phon.plugin.IPluginEntryPoint;
 import ca.phon.plugin.PhonPlugin;
+import ca.phon.ui.CommonModuleFrame;
+import ca.phon.ui.decorations.DialogHeader;
 
-@PhonPlugin(minPhonVersion = "3.1.1" )
+@PhonPlugin(minPhonVersion = "3.1.1")
 public class PhonexDebuggerEP implements IPluginEntryPoint {
 
 	public static String EP_NAME = "PhonexDebugger";
@@ -17,7 +22,36 @@ public class PhonexDebuggerEP implements IPluginEntryPoint {
 
 	@Override
 	public void pluginStart(Map<String, Object> args) {
-		// TODO
+		PhonexDebuggerWindow debugWindow = new PhonexDebuggerWindow();
+		debugWindow.pack();
+		debugWindow.setLocationByPlatform(true);
+		debugWindow.setVisible(true);
+	}
+	
+	private class PhonexDebuggerWindow extends CommonModuleFrame {
+		
+		PhonexDebugger debugger = new PhonexDebugger();
+		
+		public PhonexDebuggerWindow() {
+			super();
+			
+			setTitle("Phonex Debugger");
+			init();
+		}
+		
+		private void init() {
+			setLayout(new BorderLayout());
+			
+			DialogHeader header = new DialogHeader("Phonex Debugger", "Visualize and debug phonex expressions");
+			add(header, BorderLayout.NORTH);
+			add(debugger, BorderLayout.CENTER);
+		}
+
+		@Override
+		public void setJMenuBar(JMenuBar menubar) {
+			super.setJMenuBar(menubar);
+		}
+		
 	}
 
 }
