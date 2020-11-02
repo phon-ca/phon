144e5f1b0ee1d808b4ca39e5ea39baa1d8721de4
Greg Hedlund <greg.hedlund@gmail.com>
2020-11-02 07:52:08 -0330

    Add tutorial videos page

--- phon-manual/oxygen-webhelp/app/nav-links/json/project_manager-d62e59.js
+++
@@ -1,1 +0,0 @@
-define({"topics" : [{"title":"Corpus List","href":"project_manager\/corpora.html","attributes": {"data-id":"conceptId",},"menu": {"hasChildren":false,},"tocID":"conceptId-d62e67","topics":[]},{"title":"Session List","href":"project_manager\/sessions.html","attributes": {"data-id":"conceptId",},"menu": {"hasChildren":false,},"tocID":"conceptId-d62e75","topics":[]}]});
---
+++ phon-manual/oxygen-webhelp/app/nav-links/json/project_manager-d62e67.js
@@ -0,0 +1,1 @@
+define({"topics" : [{"title":"Corpus List","href":"project_manager\/corpora.html","attributes": {"data-id":"conceptId",},"menu": {"hasChildren":false,},"tocID":"conceptId-d62e75","topics":[]},{"title":"Session List","href":"project_manager\/sessions.html","attributes": {"data-id":"conceptId",},"menu": {"hasChildren":false,},"tocID":"conceptId-d62e83","topics":[]}]});
