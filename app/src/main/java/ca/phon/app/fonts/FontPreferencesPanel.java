54916105e17f7f3c87fab5a3f39969f960e3a39a
Greg Hedlund <greg.hedlund@gmail.com>
2021-02-17 09:48:48 -0330

    Remove dead code - remove user access to title font preference

--- app/src/main/java/ca/phon/app/fonts/FontPreferencesPanel.java
+++ app/src/main/java/ca/phon/app/fonts/FontPreferencesPanel.java
@@ -15,14 +15,9 @@
  */
 package ca.phon.app.fonts;
 
-import java.util.*;
-
 import javax.swing.*;
-import javax.swing.event.*;
 
 import org.jdesktop.swingx.*;
-
-import com.jgoodies.forms.layout.*;
 
 import ca.phon.app.prefs.*;
 import ca.phon.plugin.*;
@@ -34,13 +29,9 @@ public class FontPreferencesPanel extends PrefsPanel implements IPluginExtension
 	private static final long serialVersionUID = 5418907041332250697L;
 	
 	private HidablePanel restartPanel;
-	
-	private JSlider fontSizeSlider;
-	
+
 	private FontSelectionButton tierFontBtn;
 
-	private FontSelectionButton titleFontBtn;
-	
 	private FontSelectionButton monospaceBtn;
 	
 	public FontPreferencesPanel() {
@@ -62,63 +53,6 @@ public class FontPreferencesPanel extends PrefsPanel implements IPluginExtension
 		tierFontBtn.setTopLabelText("<html><b>Tier font</b> &#8226; Default font for tiers</html>");
 		tierFontBtn.setSelectedFont(FontPreferences.getTierFont());
 
-		titleFontBtn = new FontSelectionButton();
-		titleFontBtn.setFontProp(FontPreferences.TITLE_FONT);
-		titleFontBtn.setDefaultVal(FontPreferences.DEFAULT_TITLE_FONT);
-		titleFontBtn.setTopLabelText("<html><b>Title font</b> &#8226; Font used in some UI headers</html>");
-		titleFontBtn.setSelectedFont(FontPreferences.getTitleFont());
-
-		final Hashtable<Integer, JLabel> hashTbl = new Hashtable<Integer, JLabel>();
-		hashTbl.put(0, new JLabel("Default"));
-		
-		fontSizeSlider = new JSlider(-2, 4);
-		fontSizeSlider.setPaintTicks(true);
-		fontSizeSlider.setMajorTickSpacing(2);
-		fontSizeSlider.setSnapToTicks(true);
-		fontSizeSlider.setToolTipText("Adjust font size");
-		fontSizeSlider.setLabelTable(hashTbl);
-		fontSizeSlider.setPaintLabels(true);
-		fontSizeSlider.setValue(FontPreferences.getFontSizeIncrease());
-		fontSizeSlider.setFocusable(false);
-		fontSizeSlider.addChangeListener(new ChangeListener() {
-			
-			@Override
-			public void stateChanged(ChangeEvent e) {
-				int val = fontSizeSlider.getValue();
-				
-				if(Math.abs(val) % 2 == 0 && !fontSizeSlider.getValueIsAdjusting()) {
-					FontPreferences.setFontSizeIncrease(val);
-					
-					final Runnable later = new Runnable() {
-						
-						@Override
-						public void run() {
-//							FontPreferences.setupFontPreferences();
-						}
-						
-					};
-					SwingUtilities.invokeLater(later);
-				}
-			}
-		});
-		
-		// setup font scaler
-		final JLabel smallLbl = new JLabel("A");
-		smallLbl.setFont(getFont().deriveFont(12.0f));
-		smallLbl.setHorizontalAlignment(SwingConstants.CENTER);
-		JLabel largeLbl = new JLabel("A");
-		largeLbl.setFont(getFont().deriveFont(24.0f));
-		largeLbl.setHorizontalAlignment(SwingConstants.CENTER);
-		
-		final FormLayout fontSizeLayout = 
-				new FormLayout("fill:pref:grow(0.5), pref, fill:pref:grow, pref, fill:pref:grow(0.5)", "pref");
-		final CellConstraints cc = new CellConstraints();
-		
-		final JPanel fontSizePanel = new JPanel(fontSizeLayout);
-		fontSizePanel.add(smallLbl, cc.xy(2, 1));
-		fontSizePanel.add(fontSizeSlider, cc.xy(3, 1));
-		fontSizePanel.add(largeLbl, cc.xy(4, 1));
-
 		monospaceBtn = new FontSelectionButton();
 		monospaceBtn.setFontProp(FontPreferences.MONOSPACE_FONT);
 		monospaceBtn.setDefaultVal(FontPreferences.DEFAULT_MONOSPACE_FONT);
@@ -127,7 +61,6 @@ public class FontPreferencesPanel extends PrefsPanel implements IPluginExtension
 		
 		final JPanel uiFontsPanel = new JPanel(new VerticalLayout());
 		uiFontsPanel.add(tierFontBtn);
-		uiFontsPanel.add(titleFontBtn);
 		uiFontsPanel.add(monospaceBtn);
 		
 		uiFontsPanel.setBorder(BorderFactory.createTitledBorder("UI Fonts"));
@@ -135,7 +68,6 @@ public class FontPreferencesPanel extends PrefsPanel implements IPluginExtension
 		setLayout(new VerticalLayout());
 		add(restartPanel);
 		add(uiFontsPanel);
-//		add(fontSizePanel);
 	}
 
 	@Override
