d41b62586751cf9c2e370f470bed62cfce98c78f
Greg Hedlund <greg.hedlund@gmail.com>
2021-03-17 08:59:22 -0230

    Use 12.0f font in forms

--- script/src/main/java/ca/phon/script/params/ui/ParamPanelFactory.java
+++ script/src/main/java/ca/phon/script/params/ui/ParamPanelFactory.java
@@ -156,7 +156,7 @@ public class ParamPanelFactory extends VisitorAdapter<ScriptParam> {
 	public void visitStringScriptParam(StringScriptParam param) {
 		final JLabel paramLabel = factory.createParamLabel(param);
 		final PromptedTextField textField = factory.createStringScriptParamComponent(param);
-		textField.setFont(FontPreferences.getTierFont());
+		textField.setFont(FontPreferences.getTierFont().deriveFont(12.0f));
 		final JPanel panel = createComponentPanel(paramLabel, textField);
 		currentContainer.add(panel);
 	}
