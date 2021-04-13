6a98ac20836191063ec8eef2bb43235fdb6235aa
Greg Hedlund <greg.hedlund@gmail.com>
2021-04-13 12:29:48 -0230

    Add participant filter link to analyses and update screenshots

--- phon-manual/oxygen-webhelp/app/config.js
+++ phon-manual/oxygen-webhelp/app/config.js
@@ -105,7 +105,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20210413101406';
+                args = '20210413122855';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
