6ed411d92716926e92434d6b66736c673fbcc8f3
Greg Hedlund <greg.hedlund@gmail.com>
2021-06-30 09:18:40 -0230

    Fix implementation of 'keep like on top' (#169)

--- app/src/main/java/ca/phon/app/opgraph/nodes/table/SortNode.java
+++ app/src/main/java/ca/phon/app/opgraph/nodes/table/SortNode.java
@@ -19,6 +19,7 @@ import java.awt.*;
 import java.util.*;
 import java.util.List;
 
+import ca.hedlund.tst.TernaryTree;
 import ca.phon.app.opgraph.nodes.table.SortNodeSettings.*;
 import ca.phon.ipa.*;
 import ca.phon.opgraph.*;
@@ -26,6 +27,7 @@ import ca.phon.opgraph.app.*;
 import ca.phon.opgraph.app.extensions.*;
 import ca.phon.opgraph.exceptions.*;
 import ca.phon.query.report.datasource.*;
+import ca.phon.util.Tuple;
 
 @OpNodeInfo(
 		name="Sort",
@@ -72,7 +74,65 @@ public class SortNode extends TableOpNode implements NodeSettings {
 		List<Object[]> rowData = table.getRowData();
 		Collections.sort(rowData, new RowComparator(settings, table));
 
+		if(settings.isLikeOnTop() && settings.getSorting().size() > 1) {
+			putLikeOnTop(table);
+		}
+
 		context.put(tableOutput, table);
+	}
+
+	private void putLikeOnTop(DefaultTableDataSource table) {
+		Map<Object, Tuple<Integer, Integer>> tablePartition = partitionTable(table);
+
+		for(Object key:tablePartition.keySet()) {
+			Tuple<Integer, Integer> groupInfo = tablePartition.get(key);
+			if(groupInfo.getObj2() > 0) {
+				Object[] row = table.getRow(groupInfo.getObj2());
+				table.deleteRow(groupInfo.getObj2());
+				table.insertRow(groupInfo.getObj1(), row);
+			}
+		}
+	}
+
+	/*
+	 * Get a map of unique column values along with the first row index
+	 * they appear (data should be sorted)
+	 *
+	 * @return Map of column values (in string form) to a tuple of the startRow
+	 * for the group and the row index (if any) of the row with all columns
+	 * the same
+	 */
+	private Map<Object, Tuple<Integer, Integer>> partitionTable(DefaultTableDataSource table) {
+		Map<Object, Tuple<Integer, Integer>> retVal = new LinkedHashMap<>();
+		Object currentVal = null;
+
+		int column = table.getColumnIndex(getSortSettings().getSorting().get(0).getColumn());
+
+		for(int row = 0; row < table.getRowCount(); row++) {
+			Object val = table.getValueAt(row, column);
+			if(val == null) continue;
+			if(currentVal == null || cmp(getSortSettings().getSorting().get(0).getType(), currentVal, val) != 0) {
+				retVal.put(val, new Tuple<>(row, -1));
+				currentVal = val;
+			}
+
+			boolean allLike = true;
+			for(int sortCol = 1; sortCol < getSortSettings().getSorting().size() && allLike; sortCol++) {
+				int tcol = table.getColumnIndex(getSortSettings().getSorting().get(sortCol).getColumn());
+				Object v2 = table.getValueAt(row, tcol);
+				if(v2 == null) {
+					allLike = false;
+					continue;
+				}
+				allLike &= (cmp(getSortSettings().getSorting().get(sortCol).getType(), val, v2) == 0);
+			}
+
+			if(allLike) {
+				retVal.get(val).setObj2(row);
+			}
+		}
+
+		return retVal;
 	}
 
 	public SortNodeSettings getSortSettings() {
@@ -97,6 +157,38 @@ public class SortNode extends TableOpNode implements NodeSettings {
 
 	}
 
+	private int cmp(SortType sortType, Object v1, Object v2) throws ProcessingException {
+		int retVal = 0;
+		// if both values are numbers, sort by number
+		if((v1 instanceof Number) && (v2 instanceof Number)) {
+			Float f1 = ((Number)v1).floatValue();
+			Float f2 = ((Number)v2).floatValue();
+			retVal = f1.compareTo(f2);
+		} else {
+			String v1Txt = (v1 != null ? v1.toString() : "");
+			String v2Txt = (v2 != null ? v2.toString() : "");
+			if(sortType == SortType.PLAIN) {
+				retVal = v1Txt.compareTo(v2Txt);
+			} else if(sortType == SortType.IPA) {
+				try {
+					// HACK remove all '.' now so segmental relations results pass through
+					v1Txt = v1Txt.replaceAll("\\.", "");
+					v2Txt = v2Txt.replaceAll("\\.", "");
+					IPATranscript v1ipa =
+							(v1 != null && v1 instanceof IPATranscript ? (IPATranscript)v1 : IPATranscript.parseIPATranscript(v1Txt));
+					IPATranscript v2ipa =
+							(v2 != null && v2 instanceof IPATranscript ? (IPATranscript)v2 : IPATranscript.parseIPATranscript(v2Txt));
+
+					retVal = v1ipa.compareTo(v2ipa);
+				} catch (java.text.ParseException pe) {
+					throw new ProcessingException(null, pe);
+				}
+
+			}
+		}
+		return retVal;
+	}
+
 	private class RowComparator implements Comparator<Object[]> {
 
 		private TableDataSource table;
@@ -112,79 +204,26 @@ public class SortNode extends TableOpNode implements NodeSettings {
 		public int compare(Object[] row1, Object[] row2) {
 			int retVal = 0;
 
-			for(int i = 0; i < settings.getSorting().size(); i++) {
+			for (int i = 0; i < settings.getSorting().size(); i++) {
 				SortColumn sc = settings.getSorting().get(i);
 				final int colIdx = getColumnIndex(table, sc.getColumn());
-				if(colIdx < 0) continue;
+				if (colIdx < 0) continue;
 				final Object v1 = row1[colIdx];
 				final Object v2 = row2[colIdx];
-				
-				// when values are equals keep that row on top of list
-				if(i > 0 && i == settings.getSorting().size() - 1 && settings.isLikeOnTop()) {
-					SortColumn psc = settings.getSorting().get(i-1);
-					final int pcolidx = getColumnIndex(table, psc.getColumn());
-					final Object pv1 = row1[pcolidx];
-					final Object pv2 = row2[pcolidx];
-					
-					if(cmp(SortType.PLAIN, v2, pv2) == 0) {
-						retVal = 0;
-					} else {
-						if(cmp(SortType.PLAIN, pv1, v1) == 0) {
-							retVal = Integer.MIN_VALUE;
-						} else if(cmp(SortType.PLAIN, pv2, v2) == 0) {
-							retVal = Integer.MAX_VALUE;
-						} else {
-							retVal = cmp(sc.getType(), v1, v2);
-						}
-					}
-				} else {
-					retVal = cmp(sc.getType(), v1, v2);
-				}
-				
+
+				retVal = cmp(sc.getType(), v1, v2);
+
 				// reverse if necessary
-				if(sc.getOrder() == SortOrder.DESCENDING) {
+				if (sc.getOrder() == SortOrder.DESCENDING) {
 					retVal *= -1;
 				}
 
 				// only continue if necessary
-				if(retVal != 0) break;
+				if (retVal != 0) break;
 			}
 
 			return retVal;
 		}
-		
-		private int cmp(SortType sortType, Object v1, Object v2) throws ProcessingException {
-			int retVal = 0;
-			// if both values are numbers, sort by number
-			if((v1 instanceof Number) && (v2 instanceof Number)) {
-				Float f1 = ((Number)v1).floatValue();
-				Float f2 = ((Number)v2).floatValue();
-				retVal = f1.compareTo(f2);
-			} else {
-				String v1Txt = (v1 != null ? v1.toString() : "");
-				String v2Txt = (v2 != null ? v2.toString() : "");
-				if(sortType == SortType.PLAIN) {
-					retVal = v1Txt.compareTo(v2Txt);
-				} else if(sortType == SortType.IPA) {
-					try {
-						// HACK remove all '.' now so segmental relations results pass through
-						v1Txt = v1Txt.replaceAll("\\.", "");
-						v2Txt = v2Txt.replaceAll("\\.", "");
-						IPATranscript v1ipa =
-								(v1 != null && v1 instanceof IPATranscript ? (IPATranscript)v1 : IPATranscript.parseIPATranscript(v1Txt));
-						IPATranscript v2ipa =
-								(v2 != null && v2 instanceof IPATranscript ? (IPATranscript)v2 : IPATranscript.parseIPATranscript(v2Txt));
-
-						retVal = v1ipa.compareTo(v2ipa);
-					} catch (java.text.ParseException pe) {
-						throw new ProcessingException(null, pe);
-					}
-
-				}
-			}
-			return retVal;
-		}
-
 	}
 
 }
