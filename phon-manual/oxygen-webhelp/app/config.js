0a85cad3bcecffa89d3f668593d31be49abb09d6
Greg Hedlund <greg.hedlund@gmail.com>
2021-09-20 22:44:56 -0230

    Update landing page with tutorial videos

--- phon-manual/oxygen-webhelp/app/config.js
+++ phon-manual/oxygen-webhelp/app/config.js
@@ -105,7 +105,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20210920093407';
+                args = '20210920223012';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
