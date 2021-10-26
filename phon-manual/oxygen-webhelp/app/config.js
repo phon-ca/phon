5f8117adad9c4869610e4ef734a80c67974196f6
Greg Hedlund <greg.hedlund@gmail.com>
2021-10-26 07:51:11 -0230

    3.3.7-beta.2

--- phon-manual/oxygen-webhelp/app/config.js
+++ phon-manual/oxygen-webhelp/app/config.js
@@ -105,7 +105,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20211015122400';
+                args = '20211026074954';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
--- phon-manual/oxygen-webhelp/app/nav-links/json/project_manager-d867e67.js
+++
@@ -1,1 +0,0 @@
-define({"topics":[{"title":"Corpus List","href":"project_manager\/corpora.html","attributes":{"data-id":"conceptId"},"menu":{"hasChildren":false},"tocID":"conceptId-d867e75","topics":[]},{"title":"Session List","href":"project_manager\/sessions.html","attributes":{"data-id":"conceptId"},"menu":{"hasChildren":false},"tocID":"conceptId-d867e83","topics":[]}]});
