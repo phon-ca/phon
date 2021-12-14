1c9f6383a4b16cc9d6e5a918a9986a60b9d1dfac
Greg Hedlund <greg.hedlund@gmail.com>
2021-12-14 09:44:58 -0330

    Add rename project action

---
+++ app/src/main/java/ca/phon/app/project/actions/RenameProjectAction.java
@@ -0,0 +1,18 @@
+package ca.phon.app.project.actions;
+
+import ca.phon.app.project.ProjectWindow;
+
+import java.awt.event.ActionEvent;
+
+public class RenameProjectAction extends ProjectWindowAction {
+
+	public RenameProjectAction(ProjectWindow projectWindow) {
+		super(projectWindow);
+	}
+
+	@Override
+	public void hookableActionPerformed(ActionEvent ae) {
+
+	}
+
+}
