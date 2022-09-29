6f80c1f62742c2c3c6d69ca0b336b4325e90f416
Greg Hedlund <greg.hedlund@gmail.com>
2022-09-29 09:01:44 -0230

    3.5.0-alpha.4

--- phon-manual/oxygen-webhelp/app/config.js
+++ phon-manual/oxygen-webhelp/app/config.js
@@ -105,7 +105,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20220922144230';
+                args = '20220929090112';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
