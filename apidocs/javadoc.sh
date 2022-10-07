6cba8e4bd0f8bf1beb4bb347a7c4b96c2491e4cc
Greg Hedlund <greg.hedlund@gmail.com>
2022-10-07 09:03:41 -0230

    3.5.0-beta.1

--- apidocs/javadoc.sh
+++ apidocs/javadoc.sh
@@ -1,1 +1,1 @@
-/Library/Java/JavaVirtualMachines/jdk-15.0.2.jdk/Contents/Home/bin/javadoc @options @packages
+/Library/Java/JavaVirtualMachines/temurin-17.jdk/Contents/Home/bin/javadoc @options @packages
