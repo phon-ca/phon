8e4a337cd09d2e7adc8c0d52c692e2fc7cf54265
Greg Hedlund <greg.hedlund@gmail.com>
2022-11-07 14:44:40 -0330

    3.5.0-beta.6

--- phon-manual/oxygen-webhelp/app/nav-links/json/project_manager-d899e67.js
+++
@@ -1,1 +0,0 @@
-define({"topics":[{"title":"Corpus List","href":"project_manager\/corpora.html","attributes":{"data-id":"conceptId"},"menu":{"hasChildren":false},"tocID":"conceptId-d899e75","topics":[]},{"title":"Session List","href":"project_manager\/sessions.html","attributes":{"data-id":"conceptId"},"menu":{"hasChildren":false},"tocID":"conceptId-d899e83","topics":[]}]});
