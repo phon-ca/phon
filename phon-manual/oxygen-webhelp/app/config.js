0542642762803f992488e84b8ee53329b5672c19
Greg Hedlund <greg.hedlund@gmail.com>
2022-11-17 17:55:40 -0330

    Fix landing page

--- phon-manual/oxygen-webhelp/app/config.js
+++ phon-manual/oxygen-webhelp/app/config.js
@@ -114,7 +114,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20221117115158';
+                args = '20221117175442';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
---
+++ phon-manual/oxygen-webhelp/app/nav-links/json/timeline-d849e237.js
@@ -0,0 +1,1 @@
+define({"topics":[{"title":"Diarization (beta)","href":"session_editor\/diarization.html","attributes":{"data-id":"diarization"},"menu":{"hasChildren":false},"tocID":"diarization-d849e245","next":"diarization-d849e245"}]});
