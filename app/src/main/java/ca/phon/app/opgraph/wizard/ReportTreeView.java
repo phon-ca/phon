7f6ce7637032fb24f5e9e8c71390aacb6cc8e821
Greg Hedlund <greg.hedlund@gmail.com>
2023-02-06 06:45:53 -0330

    Remove dead code

---
+++ app/src/main/java/ca/phon/app/opgraph/wizard/ReportTreeView.java
@@ -0,0 +1,12 @@
+package ca.phon.app.opgraph.wizard;
+
+import javax.swing.*;
+
+/**
+ * Report tree viewer displayed at the end of node wizard dialogs such as the query and analysis wizards.
+ */
+public class ReportTreeView extends JPanel {
+
+
+
+}
