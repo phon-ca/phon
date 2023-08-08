283a423a6a535fe9dd31f193c24f5e1677da7e40
Greg Hedlund <greg.hedlund@gmail.com>
2023-08-08 08:58:08 -0230

    Add additional methods to MediaTimeFormatter, add display strings to MediaTimeFormatStyle

--- util/src/main/java/ca/phon/formatter/MediaTimeFormatStyle.java
+++ util/src/main/java/ca/phon/formatter/MediaTimeFormatStyle.java
@@ -1,15 +1,24 @@
 package ca.phon.formatter;
 
 public enum MediaTimeFormatStyle {
-    MINUTES_AND_SECONDS("(?:([0-9]+)\\:)?([0-9]+)\\.([0-9]*)"),
-    PADDED_MINUTES_AND_SECONDS("(?:([0-9]{1,3})\\:)?([0-9]{1,2})\\.([0-9]{1,3})"),
-    MILLISECONDS("([0-9]+)");
+    MINUTES_AND_SECONDS("Minutes and seconds", "(?:([0-9]+)\\:)?([0-9]+)\\.([0-9]*)"),
+    PADDED_MINUTES_AND_SECONDS("Padded minutes and seconds", "(?:([0-9]{1,3})\\:)?([0-9]{1,2})\\.([0-9]{1,3})"),
+    MILLISECONDS("Milliseconds", "([0-9]+)");
+
+    private String displayName;
 
     private String regex;
 
-    private MediaTimeFormatStyle(String regex) {
+    private MediaTimeFormatStyle(String displayName, String regex) {
+        this.displayName = displayName;
         this.regex = regex;
     }
 
     public String getRegex() { return this.regex; }
+
+    @Override
+    public String toString() {
+        return this.displayName;
+    }
+
 }
--- util/src/main/java/ca/phon/formatter/MediaTimeFormatter.java
+++ util/src/main/java/ca/phon/formatter/MediaTimeFormatter.java
@@ -3,13 +3,13 @@ package ca.phon.formatter;
 import java.text.ParseException;
 
 /**
- * Media time formatter for numbers
+ * Media time formatter with three formats as defined by {@link MediaTimeFormatStyle}
  */
 @FormatterType(Number.class)
 public class MediaTimeFormatter implements Formatter<Number> {
 
     /**
-     * Return times in minutes and seconds (short form: e.g., 1:3.5)
+     * Return time in minutes and seconds (short form: e.g., 1:3.5)
      *
      * @param number if number is a float time is interpreted as a values in seconds, milliseconds otherwise
      * @return formatted time value
@@ -18,22 +18,115 @@ public class MediaTimeFormatter implements Formatter<Number> {
         return timeToString(number, MediaTimeFormatStyle.MINUTES_AND_SECONDS);
     }
 
+    /**
+     * Convert seconds to string in minutes and seconds (short form: e.g., 1:3.5)
+     *
+     * @param seconds time value in seconds
+     * @return formatted time value
+     */
+    public static String secondsToMinutesAndSeconds(Number seconds) {
+        return timeToString(seconds.floatValue(), MediaTimeFormatStyle.MINUTES_AND_SECONDS);
+    }
+
+    /**
+     * Convert milliseconds to string in minutes and seconds (short form: e.g., 1:3.5)
+     *
+     * @param seconds time value in seconds
+     * @return formatted time value
+     */
+    public static String msToMinutesAndSeconds(Number ms) {
+        return timeToString(ms.longValue(), MediaTimeFormatStyle.MINUTES_AND_SECONDS);
+    }
+
+    /**
+     * Return time in minutes and seconds (long form: e.g., 001:03.05)
+     *
+     * @param number if number is a float time is interpreted as a values in seconds, milliseconds otherwise
+     * @return formatted time value
+     */
     public static String timeToPaddedMinutesAndSeconds(Number number) {
         return timeToString(number, MediaTimeFormatStyle.PADDED_MINUTES_AND_SECONDS);
     }
 
+    /**
+     * Return seconds in minutes and seconds (long form: e.g., 001:03.05)
+     *
+     * @param seconds time in seconds
+     * @return formatted time value
+     */
+    public static String secondsToPaddedMinutesAndSeconds(Number seconds) {
+        return timeToString(seconds.floatValue(), MediaTimeFormatStyle.PADDED_MINUTES_AND_SECONDS);
+    }
+
+    /**
+     * Return milliseconds in minutes and seconds (long form: e.g., 001:03.05)
+     *
+     * @param ms time in milliseconds
+     * @return formatted time value
+     */
+    public static String msToPaddedMinutesAndSeconds(Number ms) {
+        return timeToString(ms.longValue(), MediaTimeFormatStyle.PADDED_MINUTES_AND_SECONDS);
+    }
+
+    /**
+     * Return time in milliseconds (integer)
+     *
+     * @param number if number is a float time is interpreted as a values in seconds, milliseconds otherwise
+     * @return formatted time value
+     */
     public static String timeToMilliseconds(Number number) {
         return timeToString(number, MediaTimeFormatStyle.MILLISECONDS);
     }
 
+    /**
+     * Return time in milliseconds (integer)
+     *
+     * @param seconds time in seconds
+     * @return formatted time value
+     */
+    public static String secondsToMilliseconds(Number seconds) {
+        return timeToString(seconds.floatValue(), MediaTimeFormatStyle.MILLISECONDS);
+    }
+
+    /**
+     * Return time in milliseconds (integer)
+     *
+     * @param ms time in milliseconds
+     * @return formatted time value
+     */
+    public static String msToMilliseconds(Number ms) {
+        return timeToString(ms.longValue(), MediaTimeFormatStyle.MILLISECONDS);
+    }
+
+    /**
+     * Return formmated string of given number
+     *
+     * @param number if number is a float time is interpreted as a values in seconds, milliseconds otherwise
+     * @param formatStyle
+     * @return formatted time string
+     */
     public static String timeToString(Number number, MediaTimeFormatStyle formatStyle) {
         return (new MediaTimeFormatter(formatStyle)).format(number);
     }
 
+    /**
+     * Parse time to a number of seconds
+     *
+     * @param text
+     * @return number of seconds
+     * @throws ParseException
+     */
     public static float parseTimeToSeconds(String text) throws ParseException {
         return parseTimeToMilliseconds(text) / 1000.0f;
     }
 
+    /**
+     * Parse time to a number of milliseconds
+     *
+     * @param text
+     * @return number of milliseconds
+     * @throws ParseException
+     */
     public static long parseTimeToMilliseconds(String text) throws ParseException {
         long timeInMs = (new MediaTimeFormatter()).parse(text).longValue();
         return timeInMs;
