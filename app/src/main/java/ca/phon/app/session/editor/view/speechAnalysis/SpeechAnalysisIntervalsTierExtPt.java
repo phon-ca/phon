bf256bcfec279cc97f6eb6d5f1e94b8929747248
Gregory Hedlund <greg.hedlund@gmail.com>
2025-04-15 07:06:38 -0230

    Move interval tiers to bottom of speech analysis view

--- app/src/main/java/ca/phon/app/session/editor/view/speechAnalysis/SpeechAnalysisIntervalsTierExtPt.java
+++ app/src/main/java/ca/phon/app/session/editor/view/speechAnalysis/SpeechAnalysisIntervalsTierExtPt.java
@@ -2,7 +2,15 @@ package ca.phon.app.session.editor.view.speechAnalysis;
 
 import ca.phon.plugin.IPluginExtensionFactory;
 import ca.phon.plugin.IPluginExtensionPoint;
+import ca.phon.plugin.Rank;
 
+/**
+ * Extension point for creating a {@link SpeechAnalysisIntervalsTier}
+ * for the {@link SpeechAnalysisEditorView}
+ *
+ * Rank = 1000 so that is is added after the praat plug-in tiers
+ */
+@Rank(value = 1000)
 public final class SpeechAnalysisIntervalsTierExtPt implements IPluginExtensionFactory<SpeechAnalysisTier>, IPluginExtensionPoint<SpeechAnalysisTier> {
 
     @Override
