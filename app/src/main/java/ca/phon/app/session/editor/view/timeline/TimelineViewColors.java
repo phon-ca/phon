2fb98c3d96f7a0ed0f87028fe049a86457f30ebd
Gregory Hedlund <greg.hedlund@gmail.com>
2025-04-30 06:16:07 -0230

    switch to UIDefaultHandler for setting up timeline view colours

--- app/src/main/java/ca/phon/app/session/editor/view/timeline/TimelineViewColors.java
+++ app/src/main/java/ca/phon/app/session/editor/view/timeline/TimelineViewColors.java
@@ -18,7 +18,7 @@ package ca.phon.app.session.editor.view.timeline;
 import ca.phon.plugin.IPluginExtensionFactory;
 import ca.phon.plugin.IPluginExtensionPoint;
 import ca.phon.ui.theme.UIDefaults;
-import ca.phon.ui.theme.UIDefaultsHandler;H
+import ca.phon.ui.theme.UIDefaultsHandler;
 import java.awt.*;
 
 public class TimelineViewColors implements UIDefaultsHandler, IPluginExtensionPoint<UIDefaultsHandler> {
