3ab5bca2cee837ad9b34a04d817241353668bd19
Gregory Hedlund <greg.hedlund@gmail.com>
2025-09-17 06:51:18 -0230

    update PNTC script

--- query/src/main/resources/ca/phon/query/script/lib/PNTC.js
+++ query/src/main/resources/ca/phon/query/script/lib/PNTC.js
@@ -21,138 +21,139 @@
  */
 importPackage(Packages.ca.phon.ipa.features)
 importClass(Packages.ca.phon.query.script.params.DiacriticOptionsScriptParam)
-importClass(Packages.ca.phon.syllable.SyllabificationInfo)
 importClass(Packages.ca.phon.ipa.alignment.SyllableAligner)
 
 var PatternFilter = require("lib/PatternFilter").PatternFilter;
 var PatternType = require("lib/PatternFilter").PatternType;
 
 exports.PNTC = {
-	/**
-	 * Perform PNTC (percent number tones correct) calculation for an aligned pair of
-	 * IPA values
-	 *
-	 * @param word
-	 * @param features
-	 * @param ignoreDiacritics
-	 *
-	 */
-	calc_pntc: function (group) {
-		var numTarget = 0;
-		var numDeleted = 0;
-		var syllDeleted = 0;
-		var numActual = 0;
-		var numSubstituted = 0;
-		var numEpenthesized = 0;
-		var numCorrect = 0;
-		var expanded_results = new Array();
+    /**
+     * Perform PNTC (percent number tones correct) calculation for an aligned pair of
+     * IPA values
+     *
+     * @param word
+     * @param features
+     * @param ignoreDiacritics
+     *
+     */
+    calc_pntc: function (group) {
+        var numTarget = 0;
+        var numDeleted = 0;
+        var syllDeleted = 0;
+        var numActual = 0;
+        var numSubstituted = 0;
+        var numEpenthesized = 0;
+        var numCorrect = 0;
+        var expanded_results = new Array();
 
-		var targetGroup = (group.getIPATarget() == null ? new IPATranscript(): group.getIPATarget());
-		SyllabificationInfo.setupSyllabificationInfo(targetGroup);
-		var actualGroup = (group.getIPAActual() == null ? new IPATranscript(): group.getIPAActual());
-		SyllabificationInfo.setupSyllabificationInfo(actualGroup);
-		var alignment = group.getPhoneAlignment();
-		var syllAlignment = (new SyllableAligner()).calculateSyllableAlignment(targetGroup, actualGroup, alignment);
+        var targetGroup = (group.getIPATarget() == null ? new IPATranscript() : group.getIPATarget());
+        var actualGroup = (group.getIPAActual() == null ? new IPATranscript() : group.getIPAActual());
+        var alignment = group.getPhoneAlignment();
+        var syllAlignment = (new SyllableAligner()).calculateSyllableAlignment(targetGroup, actualGroup, alignment);
 
-		var targetTone = "";
-		for(var i = 0; i < targetGroup.syllables().size(); i++) {
-			var targetSyll = targetGroup.syllables().get(i);
-			var targetScInfo = targetSyll.elementAt(0).getExtension(SyllabificationInfo);
-			var targetToneNumber = (targetScInfo != null ? targetScInfo.toneNumber : "0");
-			if(targetToneNumber.length() == 0) targetToneNumber = "0";
-			targetTone += (targetTone.length > 0 ? "," : "") + targetToneNumber;
-		}
+        var targetTone = "";
+        for (var i = 0; i < targetGroup.syllables().size(); i++) {
+            var targetSyll = targetGroup.syllables().get(i);
+            var targetScInfo = targetSyll.elementAt(0).syllableInfo();
+            var targetToneNumber = targetScInfo.tone() != null ? targetScInfo.tone().toString() : "";
+            targetTone += (i > 0 ? "," : "") + targetToneNumber;
+        }
 
-		var actualTone = "";
-		for(var i = 0; i < actualGroup.syllables().size(); i++) {
-			var actualSyll = actualGroup.syllables().get(i);
-			var actualScInfo = actualSyll.elementAt(0).getExtension(SyllabificationInfo);
-			var actualToneNumber = (actualScInfo != null ? actualScInfo.toneNumber : "0");
-			if(actualToneNumber.length() == 0) actualToneNumber = "0";
-			actualTone += (actualTone.length > 0 ? "," : "") + actualToneNumber;
-		}
-		
-		numTarget = targetGroup.syllables().size();
-		numActual = actualGroup.syllables().size();
-		
-		for(var i = 0; i < syllAlignment.getAlignmentLength(); i++) {
-			var alignedSylls = syllAlignment.getAlignedElements(i);
-			
-			var targetSyll = alignedSylls.get(0);
-			var actualSyll = alignedSylls.get(1);
-			
-			var wasCorrect = false;
-			var wasSub = false;
-			var wasDeleted = false;
-			var wasEpen = false;
-			var wasSyllDeleted = false;
-			
-			if(targetSyll == null) {
-				wasEpen = true;
-			} else if(actualSyll == null) {
-				wasDeleted = true;
-				wasSyllDeleted = true;
-			} else {
-				var targetScInfo = targetSyll.elementAt(0).getExtension(SyllabificationInfo);
-				var targetToneNumber = (targetScInfo != null ? targetScInfo.toneNumber : "");
-				
-				var actualScInfo = actualSyll.elementAt(0).getExtension(SyllabificationInfo);
-				var actualToneNumber = (actualScInfo != null ? actualScInfo.toneNumber : "");
-				
-				if(targetToneNumber == actualToneNumber) {
-					wasCorrect = true;
-				} else {
-					if(targetToneNumber.length() > 0 && actualToneNumber.length() == 0) {
-						wasDeleted = true;
-					} else {
-						wasSub = true;
-					}
-				}
-			}
-			
-			var expandedResult = {
-				target: targetSyll,
-				actual: actualSyll,
-				syllDeleted: (wasSyllDeleted == true ? 1 : 0),
-				correct: (wasCorrect == true ? 1 : 0),
-				substituted: (wasSub == true ? 1 : 0),
-				deleted: (wasDeleted == true ? 1 : 0),
-				epen: (wasEpen == true ? 1 : 0)
-			};
-			expanded_results.push(expandedResult);
-			
-			if(wasCorrect == true) 
-				++numCorrect;
-			if(wasSub == true)
-				++numSubstituted;
-			if(wasDeleted == true) 
-				++numDeleted;
-			if(wasSyllDeleted == true)
-				++syllDeleted;
-			if(wasEpen == true)
-				++numEpenthesized;
-		}
+        var actualTone = "";
+        for (var i = 0; i < actualGroup.syllables().size(); i++) {
+            var actualSyll = actualGroup.syllables().get(i);
+            var actualScInfo = actualSyll.elementAt(0).syllableInfo();
+            var actualToneNumber = actualScInfo.tone() != null ? actualScInfo.tone().toString() : "";
+            actualTone += (actualTone.length > 0 ? "," : "") + actualToneNumber;
+        }
 
-		var retVal = {
-			target: numTarget,
-			actual: numActual,
-			targetTone: targetTone,
-			actualTone: actualTone,
-			correct: numCorrect,
-			substituted: numSubstituted,
-			deleted: numDeleted,
-			syllDeleted: syllDeleted,
-			epen: numEpenthesized,
-			expanded_results: expanded_results
-		};
-		return retVal;
-	}
-	
+        numTarget = targetGroup.syllables().size();
+        numActual = actualGroup.syllables().size();
+
+        for (var i = 0; i < syllAlignment.getAlignmentLength(); i++) {
+            var alignedSylls = syllAlignment.getAlignedElements(i);
+
+            var alignedTargetSyll = alignedSylls.get(0);
+            var alignedActualSyll = alignedSylls.get(1);
+
+            var wasCorrect = false;
+            var wasSub = false;
+            var wasDeleted = false;
+            var wasEpen = false;
+            var wasSyllDeleted = false;
+
+            if (targetSyll == null) {
+                wasEpen = true;
+            } else if (actualSyll == null) {
+                wasDeleted = true;
+                wasSyllDeleted = true;
+            } else {
+                var alignedTargetScInfo = alignedTargetSyll.elementAt(0).syllableInfo();
+                var alignedTargetToneNumber = alignedTargetScInfo.tone();
+
+                var alignedActualScInfo = alignedActualSyll.elementAt(0).syllableInfo();
+                var alignedActualToneNumber = alignedActualScInfo.tone();
+
+                if (alignedTargetToneNumber === null && alignedActualToneNumber === null) {
+                    wasCorrect = true;
+                } else if (alignedTargetToneNumber === null && alignedActualToneNumber !== null) {
+                    wasSub = true;
+                } else if (alignedTargetToneNumber !== null && alignedActualToneNumber === null) {
+                    wasDeleted = true;
+                } else if (alignedTargetToneNumber !== null && alignedActualToneNumber !== null) {
+                    if(alignedTargetToneNumber.toString() === alignedActualToneNumber.toString()) {
+                        wasCorrect = true;
+                    } else {
+                        wasSub = true;
+                    }
+                } else {
+                    wasSub = true;
+                }
+            }
+
+            var expandedResult = {
+                target: alignedTargetSyll,
+                actual: alignedActualSyll,
+                syllDeleted: (wasSyllDeleted === true ? 1 : 0),
+                correct: (wasCorrect === true ? 1 : 0),
+                substituted: (wasSub === true ? 1 : 0),
+                deleted: (wasDeleted === true ? 1 : 0),
+                epen: (wasEpen === true ? 1 : 0)
+            };
+            expanded_results.push(expandedResult);
+
+            if (wasCorrect === true)
+                ++numCorrect;
+            if (wasSub === true)
+                ++numSubstituted;
+            if (wasDeleted === true)
+                ++numDeleted;
+            if (wasSyllDeleted === true)
+                ++syllDeleted;
+            if (wasEpen === true)
+                ++numEpenthesized;
+        }
+
+        var retVal = {
+            target: numTarget,
+            actual: numActual,
+            targetTone: targetTone,
+            actualTone: actualTone,
+            correct: numCorrect,
+            substituted: numSubstituted,
+            deleted: numDeleted,
+            syllDeleted: syllDeleted,
+            epen: numEpenthesized,
+            expanded_results: expanded_results
+        };
+        return retVal;
+    }
+
 };
 
 exports.PNTCOptions = function (id, aligned) {
 
-	this.param_setup = function (params) {
-	};
+    this.param_setup = function (params) {
+    };
 
 };
