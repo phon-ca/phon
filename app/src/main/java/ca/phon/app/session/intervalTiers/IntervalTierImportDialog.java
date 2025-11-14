185e5fae4b4099ac74d21ab36fba30c1ffaa88a1
Gregory Hedlund <greg.hedlund@gmail.com>
2025-11-14 09:00:34 -0330

    give dialog a min size

--- app/src/main/java/ca/phon/app/session/intervalTiers/IntervalTierImportDialog.java
+++ app/src/main/java/ca/phon/app/session/intervalTiers/IntervalTierImportDialog.java
@@ -80,6 +80,7 @@ public class IntervalTierImportDialog<T, R extends IntervalTierImporter> extends
         setLayout(new BorderLayout());
 
         setModal(false);
+        setMinimumSize(new Dimension(500, 400));
 
         // Header
         DialogHeader header = new DialogHeader(title, description);
